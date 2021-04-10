125133284ad7d65539b4e91064b4522e2e7055a8
Adam Landis <pluckedowl@gmail.com>
2021-04-09 18:22:16 -0700

    nvim: add mappings for merge conflicts

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,3 +1,6 @@
 local utils = require('utils')
 
 utils.map('n', '<leader>gs', '<cmd>Gstatus<CR>')
+
+utils.map('n', '<leader>gf', '<cmd>diffget //2<CR>')
+utils.map('n', '<leader>gj', '<cmd>diffget //3<CR>')
