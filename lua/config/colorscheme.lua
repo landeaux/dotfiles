3df1bf71c50e751eeeee4489ffe773f36086bbd2
Adam Landis <pluckedowl@gmail.com>
2021-04-10 16:00:04 -0700

    nvim: change method of getting dracula palette

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,27 +1,37 @@
-local colorscheme = { }
 local utils = require('utils')
+
 local cmd = vim.cmd
+local colorscheme = { }
 
 utils.opt('o', 'termguicolors', true)
 
 -- DRACULA --
 cmd('colorscheme dracula')
 
+local function get_dracula_palette_hex(key)
+  return vim.g['dracula#palette'][key][1]
+end
+
 colorscheme.dracula = {
-  red = utils.get_hi_term('DraculaRed', 'guifg'),
-  pink = utils.get_hi_term('DraculaPink', 'guifg'),
-  orange = utils.get_hi_term('DraculaOrange', 'guifg'),
-  yellow = utils.get_hi_term('DraculaYellow', 'guifg'),
-  green = utils.get_hi_term('DraculaGreen', 'guifg'),
-  cyan = utils.get_hi_term('DraculaCyan', 'guifg'),
-  purple = utils.get_hi_term('DraculaPurple', 'guifg'),
-  fg = utils.get_hi_term('DraculaFg', 'guifg'),
-  bg_light = utils.get_hi_term('DraculaBgLight', 'guibg'),
-  bg_lighter = utils.get_hi_term('DraculaBgLighter', 'guibg'),
-  bg_dark = utils.get_hi_term('DraculaBgDark', 'guibg'),
-  bg_darker = utils.get_hi_term('DraculaBgDarker', 'guibg'),
-  subtle = utils.get_hi_term('DraculaSubtle', 'guifg'),
-  comment = utils.get_hi_term('DraculaComment', 'guifg'),
+  fg = get_dracula_palette_hex('fg'),
+
+  bg_lighter = get_dracula_palette_hex('bglighter'),
+  bg_light = get_dracula_palette_hex('bglight'),
+  bg = get_dracula_palette_hex('bg'),
+  bg_dark = get_dracula_palette_hex('bgdark'),
+  bg_darker = get_dracula_palette_hex('bgdarker'),
+
+  comment = get_dracula_palette_hex('comment'),
+  selection = get_dracula_palette_hex('selection'),
+  subtle = get_dracula_palette_hex('subtle'),
+
+  cyan = get_dracula_palette_hex('cyan'),
+  green = get_dracula_palette_hex('green'),
+  orange = get_dracula_palette_hex('orange'),
+  pink = get_dracula_palette_hex('pink'),
+  purple = get_dracula_palette_hex('purple'),
+  red = get_dracula_palette_hex('red'),
+  yellow = get_dracula_palette_hex('yellow'),
 }
 
 -- GRUVBOX --
