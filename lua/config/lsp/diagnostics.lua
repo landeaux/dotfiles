c515a95548402988c1e07bc27f87eb7b3aa940d4
Adam Landis <pluckedowl@gmail.com>
2021-04-15 12:37:42 -0700

    nvim: remove unused config

--- lua/config/lsp/diagnostics.lua
+++ lua/config/lsp/diagnostics.lua
@@ -32,10 +32,10 @@ vim.fn.sign_define('LspDiagnosticsSignError',
     { text = '✗', texthl = 'LspDiagnosticsSignError' })
 
 vim.fn.sign_define('LspDiagnosticsSignWarning',
-    { text = '', texthl = 'LspDiagnosticsSignWarning' })
+    { text = '', texthl = 'LspDiagnosticsSignWarning' })
 
 vim.fn.sign_define('LspDiagnosticsSignInformation',
-    { text = '', texthl = 'LspDiagnosticsSignInformation' })
+    { text = 'כֿ', texthl = 'LspDiagnosticsSignInformation' })
 
 vim.fn.sign_define('LspDiagnosticsSignHint',
-    { text = '', texthl = 'LspDiagnosticsSignHint' })
+    { text = '', texthl = 'LspDiagnosticsSignHint' })
