425c53adbbe20c308c0d0a70376334e03e345da6
Adam Landis <pluckedowl@gmail.com>
2021-04-15 14:15:27 -0700

    nvim: add augroup for custom filetypes

--- lua/settings.lua
+++ lua/settings.lua
@@ -102,6 +102,19 @@ require('utils').create_augroup({
     {'TextYankPost', '*', 'silent!', 'lua vim.highlight.on_yank()'}
 }, 'highlight_on_yank')
 
+require('utils').create_augroup({
+    {'BufRead,BufNewFile', '.env.*', 'set filetype=sh'},
+    {'BufRead,BufNewFile', '.gitconfig.local', 'set filetype=gitconfig'},
+    {'BufRead,BufNewFile', '.pylintrc', 'set filetype=conf'},
+    {'BufRead,BufNewFile', '.tmux.conf.local', 'set filetype=tmux'},
+    {'BufRead,BufNewFile', '.zshrc.local', 'set filetype=sh'},
+    {'BufRead,BufNewFile', 'Dockerfile.*', 'set filetype=dockerfile'},
+    {'BufRead,BufNewFile', 'Dockerfile-*', 'set filetype=dockerfile'},
+    {'BufRead,BufNewFile', 'gitconfig', 'set filetype=gitconfig'},
+    {'BufRead,BufNewFile', 'gitignore', 'set filetype=conf'},
+    {'BufRead,BufNewFile', '*.graphql,*.graphqls,*.gql', 'set filetype=graphql'},
+}, 'custom_filetypedetect')
+
 -- Enable syntax highlighting
 cmd 'syntax enable'
 
