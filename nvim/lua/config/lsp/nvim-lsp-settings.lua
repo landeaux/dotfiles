0858440785bb6e5938ee8db356e5e9172ca72493
landeaux <pluckedowl@gmail.com>
2021-04-28 02:15:23 -0700

    nvim: add LspSaga config, remove universal formatting

--- nvim/lua/config/lsp/nvim-lsp-settings.lua
+++ nvim/lua/config/lsp/nvim-lsp-settings.lua
@@ -1,9 +1,53 @@
-local nvim_lsp = require('lspconfig')
+vim.lsp.protocol.CompletionItemKind = {
+    "   (Text) ",
+    "   (Method)",
+    "   (Function)",
+    "   (Constructor)",
+    " ﴲ  (Field)",
+    "[] (Variable)",
+    "   (Class)",
+    " ﰮ  (Interface)",
+    "   (Module)",
+    " 襁 (Property)",
+    "   (Unit)",
+    "   (Value)",
+    " 練 (Enum)",
+    "   (Keyword)",
+    " ﬌  (Snippet)",
+    "   (Color)",
+    "   (File)",
+    "   (Reference)",
+    "   (Folder)",
+    "   (EnumMember)",
+    " ﲀ  (Constant)",
+    " ﳤ  (Struct)",
+    "   (Event)",
+    "   (Operator)",
+    "   (TypeParameter)"
+}
 
-local M = {}
+local function documentHighlight(client, bufnr)
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec(
+            [[
+      hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
+      hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
+      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+            false
+        )
+    end
+end
 
--- Default on_attach for LSP servers
-local on_attach = function(client, bufnr)
+local lsp_config = {}
+
+function lsp_config.common_on_attach(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
@@ -13,7 +57,8 @@ local on_attach = function(client, bufnr)
     local opts = { noremap=true, silent=true }
     buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'K', ':Lspsaga hover_doc<CR>', opts)
+    -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '<Leader>lwa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
@@ -21,9 +66,18 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<Leader>lwl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
     buf_set_keymap('n', '<Leader>lD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
     buf_set_keymap('n', '<Leader>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<Leader>lc', ':Lspsaga code_action<CR>', opts)
     buf_set_keymap('n', '<Leader>lds', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "single" })<CR>', opts)
-    buf_set_keymap('n', '<Leader>ldp', '<cmd>lua vim.lsp.diagnostic.goto_prev({ popup_opts = { border = "single" }})<CR>', opts)
-    buf_set_keymap('n', '<Leader>ldn', '<cmd>lua vim.lsp.diagnostic.goto_next({ popup_opts = { border = "single" }})<CR>', opts)
+    buf_set_keymap('n', '<Leader>ldp', ':Lspsaga diagnostic_jump_prev<CR>', opts)
+    buf_set_keymap('n', '<Leader>ldn', ':Lspsaga diagnostic_jump_next<CR>', opts)
+
+    -- vim.cmd('nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>')
+    -- scroll down hover doc or scroll in definition preview
+    -- buf_set_keymap('n', '<C-f>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>")
+    -- scroll up hover doc
+    -- buf_set_keymap('n', '<C-b>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>")
+
+    vim.cmd('command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").toggle()')
 
     local function buf_bind_picker(...)
         require('config.tools.telescope-nvim-utils').buf_bind_picker(bufnr, ...)
@@ -34,11 +88,15 @@ local on_attach = function(client, bufnr)
     buf_bind_picker('<Leader>lsw', 'lsp_workspace_symbols')
     buf_bind_picker('<Leader>ldd', 'lsp_document_diagnostics')
     buf_bind_picker('<Leader>ldw', 'lsp_workspace_diagnostics')
-    buf_bind_picker('<Leader>lc', 'lsp_code_actions')
+    buf_bind_picker('<Leader>lad', 'lsp_code_actions')
 
     local keys = {
         l = {
             name = '+lsp',
+            a = {
+                name = '+code-actions',
+                d = 'Document Actions'
+            },
             s = {
                 name = '+symbols',
                 d = 'Document Symbols',
@@ -64,93 +122,26 @@ local on_attach = function(client, bufnr)
         }
     }
 
-    if client.resolved_capabilities.document_formatting then
-        buf_set_keymap("n", "<space>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-
-        -- Format on save
-        require('utils').create_augroup({
-            {'BufWritePre', '*', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
-        }, 'lsp_auto_format')
-
-        keys.l.f = 'Format'
-
-        if client.resolved_capabilities.document_range_formatting then
-            buf_set_keymap('n', '<space>lF',
-                '<cmd>lua vim.lsp.buf.range_formatting()<CR>',
-                opts
-            )
-
-            keys.l.F = 'Range Format'
-        end
-
-    elseif client.resolved_capabilities.document_range_formatting then
-        buf_set_keymap(
-            'n', '<space>lf',
-            '<cmd>lua vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})<CR>',
-            opts
-        )
-        buf_set_keymap('n', '<space>lF',
-            '<cmd>lua vim.lsp.buf.range_formatting()<CR>',
-            opts
-        )
-
-        -- Format on save
-        require('utils').create_augroup({
-            {
-                'BufWritePre', '*',
-                'lua vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})'
-            }
-        }, 'lsp_auto_format')
-
-        keys.l.f = 'Format'
-        keys.l.F = 'Range Format'
-    end
-
     require('whichkey_setup').register_keymap('leader', keys)
 
-    -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec([[
-        hi! link LspReferenceRead DraculaSelection
-        hi! link LspReferenceText DraculaSelection
-        hi! link LspReferenceWrite DraculaSelection
-        augroup lsp_document_highlight
-            autocmd! * <buffer>
-            autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-            autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        augroup END
-        ]], false)
-    end
+    documentHighlight(client, bufnr)
 
     -- LSP Signatures
     require('lsp_signature').on_attach()
-
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-        vim.lsp.handlers.hover,
-        {
-          border = "single"
-        }
-    )
-
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-        vim.lsp.handlers.signature_help,
-        {
-          border = "single"
-        }
-    )
 end
 
--- LSP Server Configurations
-M.default_config = function()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true;
-
-    return {
-        capabilities = capabilities,
-        on_attach = on_attach,
-    }
+function lsp_config.tsserver_on_attach(client, bufnr)
+    lsp_config.common_on_attach(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
 end
 
-M.common_on_attach = on_attach
+function lsp_config.vuels_on_attach(client, bufnr)
+    lsp_config.common_on_attach(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+end
 
-return M
+-- Use a loop to conveniently both setup defined servers
+-- and map buffer local keybindings when the language server attaches
+-- local servers = {"pyright", "tsserver"}
+-- for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {on_attach = on_attach} end
+return lsp_config
--- nvim/lua/config/lsp/python-ls.lua
+++ nvim/lua/config/lsp/python-ls.lua
@@ -1,3 +1,4 @@
+-- npm i -g pyright
 require'lspconfig'.pyright.setup {
     cmd = {vim.fn.stdpath('data') .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
     on_attach = require'config.lsp.nvim-lsp-settings'.common_on_attach,
