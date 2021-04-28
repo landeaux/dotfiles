d7a28784e2be052a425abeb7f017563423837ed6
Adam Landis <pluckedowl@gmail.com>
2021-04-28 10:40:34 -0700

    nvim: add lua lsp config

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -1,7 +1,14 @@
+-- Language servers
 require('config.lsp.nvim-lsp-settings')
 require('config.lsp.python-ls')
+require('config.lsp.lua-ls')
+
+-- Formatting
 require('config.lsp.efm-general-ls')
+
 require('config.lsp.diagnostics')
+
+-- LSP plugin config
 require('config.lsp.nvim-compe')
 -- require('config.lsp.lspkind')
 require('config.lsp.nvim-lightbulb')
