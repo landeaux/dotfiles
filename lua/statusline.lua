eb43adf7c8a12406a372a1b2155bf9f724bcb265
landeaux <pluckedowl@gmail.com>
2021-04-27 22:49:51 -0700

    don't show Lsp info for startify, empty buffers

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -122,7 +122,7 @@ gls.right[5] = {
     ShowLspClient = {
         provider = 'GetLspClient',
         condition = function()
-            local tbl = {['dashboard'] = true, [' '] = true}
+            local tbl = {['startify'] = true, [' '] = true, [''] = true}
             if tbl[vim.bo.filetype] then return false end
             return true
         end,
