a7c8bd36e03c5e90ff3e430fa37a7ea6334a964f
Adam Landis <pluckedowl@gmail.com>
2021-05-14 09:13:14 -0700

    nvim: use prettier_d_slim & eslint_d for efm

--- nvim/lua/config/lsp/efm-general-ls.lua
+++ nvim/lua/config/lsp/efm-general-ls.lua
@@ -79,22 +79,33 @@ local prettier = {
             --single-quote true \
             --tab-width 2 \
             --trailing-comma es5 \
-            --config-precedence prefer-file \
             --stdin-filepath ${INPUT}
 	]],
     formatStdin = true
 }
 
+-- local prettier = {
+--     formatCommand = [[
+--         prettier_d_slim \
+--             --semi false \
+--             --single-quote true \
+--             --tab-width 2 \
+--             --trailing-comma es5 \
+--             --stdin --stdin-filepath ${INPUT}
+--     ]],
+--     formatStdin = true
+-- }
+
 -- You can look for project scope Prettier and Eslint with e.g. vim.fn.glob("node_modules/.bin/prettier") etc. If it is not found revert to global Prettier where needed.
 
--- local prettier = {formatCommand = "./node_modules/.bin/prettier --stdin-filepath ${INPUT}", formatStdin = true}
-
 local eslint = {
-    lintCommand = "./node_modules/.bin/eslint -f unix --stdin --stdin-filename ${INPUT}",
+    -- lintCommand = "./node_modules/.bin/eslint -f unix --stdin --stdin-filename ${INPUT}",
+    lintCommand = "eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}",
     lintIgnoreExitCode = true,
     lintStdin = true,
     lintFormats = {"%f(%l,%c): %tarning %m", "%f(%l,%c): %rror %m"},
-    formatCommand = "./node_modules/.bin/eslint --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+    -- formatCommand = "./node_modules/.bin/eslint --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+    formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
     formatStdin = true
 }
 
