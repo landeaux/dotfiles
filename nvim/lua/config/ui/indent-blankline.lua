50b6c92f51c52762ef8245f12558605a45bab489
Adam Landis <pluckedowl@gmail.com>
2021-05-18 18:43:17 -0700

    nvim: add highlight for blankline char

--- nvim/lua/config/ui/indent-blankline.lua
+++ nvim/lua/config/ui/indent-blankline.lua
@@ -3,6 +3,7 @@ local g = vim.g
 g.indent_blankline_enabled = true
 
 g.indent_blankline_char = '▏' -- │
+g.indent_blankline_char_highlight = 'LineNr'
 g.indent_blankline_space_char = ' '
 
 g.indent_blankline_indent_level = 10
