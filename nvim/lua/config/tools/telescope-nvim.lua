c2e3e38b825a26b424af569391a8fca4aecc81dd
Adam Landis <pluckedowl@gmail.com>
2021-05-18 19:00:48 -0700

    nvim: use fzy_sorter for telescope generic sorter

--- nvim/lua/config/tools/telescope-nvim.lua
+++ nvim/lua/config/tools/telescope-nvim.lua
@@ -36,7 +36,7 @@ require('telescope').setup{
         file_sorter =  require'telescope.sorters'.get_fzy_sorter,
         file_ignore_patterns = {},
 
-        generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+        generic_sorter = require'telescope.sorters'.get_fzy_sorter,
         shorten_path = true,
         winblend = 0,
         width = 0.75,
