ddd2806310967a98c8e26dd0fce06d279b397b94
landeaux <pluckedowl@gmail.com>
2021-06-01 08:55:19 -0700

    nvim: add custom python config for remote attach

--- nvim/lua/config/dap/configurations.lua
+++ nvim/lua/config/dap/configurations.lua
@@ -1,1 +1,32 @@
+local dap = require('dap')
 
+dap.configurations.python = {
+  {
+    type = 'python';
+    request = 'attach';
+    name = 'Attach remote (custom)';
+    cwd = vim.fn.getcwd();
+    host = function()
+        local value = vim.fn.input('Host [127.0.0.1]: ')
+        if value ~= '' then
+            return value
+        end
+        return '127.0.0.1'
+    end;
+    port = function()
+        return tonumber(vim.fn.input('Port [5678]: ')) or 5678
+    end;
+    pathMappings = function()
+        local remoteRoot = vim.fn.input('Remote Root [/]: ')
+        if remoteRoot == '' then
+            remoteRoot = '/'
+        end;
+        return {
+            {
+                localRoot = vim.fn.getcwd();
+                remoteRoot = remoteRoot;
+            };
+        };
+    end;
+  },
+}
