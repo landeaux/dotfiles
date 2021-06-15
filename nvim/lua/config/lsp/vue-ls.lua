1c4cf6aaac4cd2c21685e5a291c50ddb14e3eeba
Adam Landis <pluckedowl@gmail.com>
2021-06-15 14:00:55 -0700

    nvim: update vue lsp config

--- nvim/lua/config/lsp/vue-ls.lua
+++ nvim/lua/config/lsp/vue-ls.lua
@@ -1,4 +1,49 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 require'lspconfig'.vuels.setup {
-    cmd = {vim.fn.stdpath("data") .. "/lspinstall/vue/node_modules/.bin/vls"},
-    on_attach = require'config.lsp.nvim-lsp-settings'.vuels_on_attach
+    cmd = {vim.fn.stdpath("data") .. "/lspinstall/vue/node_modules/.bin/vls", "--stdio"},
+    on_attach = require'config.lsp.nvim-lsp-settings'.vuels_on_attach,
+    capabilities = capabilities,
+    settings = {
+        vetur = {
+            ignoreProjectWarning = false,
+            useWorkspaceDependencies = true,
+            validation = {
+                template = true,
+                templateProps = false,
+                interpolation = true,
+                style = true,
+                script = true
+            },
+            completion = {
+                autoImport = true,
+                tagCasing = 'initial',
+                scaffoldSnippetSources = {workspace = '', user = '', vetur = '﵂'}
+            },
+            grammar = {customBlocks = {}},
+            format = {
+                enable = false,
+                options = {tabSize = 2, useTabs = false},
+                defaultFormatter = {
+                    html = {"prettier"},
+                    js = {"prettier-eslint"},
+                    sass = {"sass-formatter"}
+                },
+                defaultFormatterOptions = {},
+                scriptInitialIndent = false,
+                styleInitialIndent = false
+            },
+            languageFeatures = {codeActions = true, updateImportOnFileMove = true},
+            trace = {server = 'off'},
+            dev = {vlsPath = '', vlsPort = -1, logLevel = 'INFO'},
+            experimental = {templateInterpolationService = false}
+        },
+        css = {},
+        html = {suggest = {}},
+        javascript = {format = {}},
+        typescript = {tsdk = nil, format = {}},
+        emmet = {},
+        stylusSupremacy = {}
+    }
 }
