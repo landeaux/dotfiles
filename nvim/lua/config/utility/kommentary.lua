49d8e74ce4815c365887e995cef33344aa977194
landeaux <pluckedowl@gmail.com>
2021-06-20 15:17:57 -0700

    nvim: configure js, ts, react language for kommentary

--- nvim/lua/config/utility/kommentary.lua
+++ nvim/lua/config/utility/kommentary.lua
@@ -2,19 +2,27 @@ local bind = vim.api.nvim_set_keymap
 local config = require('kommentary.config')
 local wk = require('whichkey_setup')
 
+local js_ts_react_config = {
+    prefer_single_line_comments = true,
+    single_line_comment_string = 'auto',
+    multi_line_comment_strings = 'auto',
+}
+config.configure_language('javascript', js_ts_react_config)
+config.configure_language('typescript', js_ts_react_config)
+config.configure_language('javascriptreact', js_ts_react_config)
+config.configure_language('typescriptreact', js_ts_react_config)
 config.configure_language(
-    "lua", {single_line_comment_string = '--', prefer_single_line_comments = true}
-)
-
-require("kommentary.config").configure_language(
-    "vue", {
+    'vue', {
         hook_function = function()
             require('ts_context_commentstring.internal').update_commentstring()
         end,
-        single_line_comment_string = "auto",
-        multi_line_comment_strings = "auto",
+        single_line_comment_string = 'auto',
+        multi_line_comment_strings = 'auto',
         prefer_single_line_comments = true,
     }
+)
+config.configure_language(
+    'lua', {single_line_comment_string = '--', prefer_single_line_comments = true}
 )
 
 bind('n', '<leader>/', '<Plug>kommentary_line_default', {})
