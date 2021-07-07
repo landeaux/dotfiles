4582d3f9f787c564b2e554a516788baeb4d30a8f
landeaux <pluckedowl@gmail.com>
2021-07-06 23:13:12 -0700

    nvim: improve nvim-tree icons

--- nvim/lua/config/tools/nvim_tree.lua
+++ nvim/lua/config/tools/nvim_tree.lua
@@ -23,11 +23,22 @@ g.nvim_tree_add_trailing = 0
 g.nvim_tree_group_empty = 1
 g.nvim_tree_lsp_diagnostics = 1
 g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
+
 g.nvim_tree_icons = {
     default = "",
     symlink = "",
-    git = { unstaged = "✗", staged = "✓", unmerged = "", renamed = "➜", untracked = "★" },
+    git = {
+        unstaged = "",
+        staged = "✓",
+        unmerged = "",
+        renamed = "➜",
+        untracked = "★",
+        deleted = "✗",
+        ignored = "◌",
+    },
     folder = {
+        arrow_open = "",
+        arrow_closed = "",
         default = "",
         open = "",
         empty = "",
