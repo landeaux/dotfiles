a57310bca2aa6cbb25545a7ce8f33bb8f8747d10
landeaux <pluckedowl@gmail.com>
2021-07-15 23:53:24 -0700

    nvim: add capabilities to dockerls

--- nvim/lua/config/lsp/docker-ls.lua
+++ nvim/lua/config/lsp/docker-ls.lua
@@ -1,9 +1,13 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 require("lspconfig").dockerls.setup({
     cmd = {
         vim.fn.stdpath("data") .. "/lspinstall/dockerfile/node_modules/.bin/docker-langserver",
         "--stdio",
     },
     on_attach = require("config.lsp.nvim-lsp-settings").common_on_attach,
+    capabilities = capabilities,
     flags = {
         debounce_text_changes = 150,
     },
