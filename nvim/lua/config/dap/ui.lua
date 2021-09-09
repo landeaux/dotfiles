2815621a961bcce7232072ce12555e89f13cad13
Adam Landis <pluckedowl@gmail.com>
2021-09-09 09:59:15 -0700

    nvim: update dap-ui config for breaking changes

--- nvim/lua/config/dap/ui.lua
+++ nvim/lua/config/dap/ui.lua
@@ -6,24 +6,36 @@ require("dapui").setup({
         open = "o",
         remove = "d",
         edit = "e",
+        repl = "r",
     },
     sidebar = {
+        open_on_start = true,
+        -- You can change the order of elements in the sidebar
         elements = {
-            -- You can change the order of elements in the sidebar
-            "scopes",
-            "stacks",
-            "watches",
+            -- Provide as ID strings or tables with "id" and "size" keys
+            {
+                id = "scopes",
+                size = 0.25, -- Can be float or integer > 1
+            },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks", size = 0.25 },
+            { id = "watches", size = 0.25 },
         },
-        width = 40,
-        position = "left", -- Can be "left" or "right"
+        size = 40,
+        position = "left", -- Can be "left", "right", "top", "bottom"
     },
     tray = {
+        open_on_start = true,
         elements = { "repl" },
-        height = 10,
-        position = "bottom", -- Can be "bottom" or "top"
+        size = 10,
+        position = "bottom", -- Can be "left", "right", "top", "bottom"
     },
     floating = {
         max_height = nil, -- These can be integers or a float between 0 and 1.
         max_width = nil, -- Floats will be treated as percentage of your screen.
+        mappings = {
+            close = { "q", "<Esc>" },
+        },
     },
+    windows = { indent = 1 },
 })
