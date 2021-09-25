fecb6a10a0d77b332f8bb9ae1c845151532ce81b
landeaux <pluckedowl@gmail.com>
2021-09-25 12:39:31 -0700

    nvim: update nvim-tree config to use setup()

--- nvim/lua/config/tools/nvim_tree.lua
+++ nvim/lua/config/tools/nvim_tree.lua
@@ -3,26 +3,22 @@ local g = vim.g
 local bind = vim.api.nvim_set_keymap
 
 g.nvim_tree_side = "left"
-g.nvim_tree_width = 40
+g.nvim_tree_width = 40 -- can be width_in_columns or 'width_in_percent%'
 g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
 g.nvim_tree_gitignore = 1
-g.nvim_tree_auto_open = 1
-g.nvim_tree_auto_close = 1
+g.nvim_tree_quit_on_open = 1 -- closes the tree when you open a file
+g.nvim_tree_indent_markers = 1 -- shows indent markers when folders are open
+g.nvim_tree_hide_dotfiles = 1 -- hides files and folders starting with a dot `.`
+g.nvim_tree_git_hl = 1 -- enable file highlight for git attributes (can be used without the icons)
+g.nvim_tree_highlight_opened_files = 1 -- enable folder and file icon highlight for opened files/directories.
+g.nvim_tree_root_folder_modifier = ":~" -- see :help filename-modifiers for more options
+g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
+g.nvim_tree_group_empty = 1 -- compact folders that only contain a single folder into one node in the file tree
+g.nvim_tree_lsp_diagnostics = 1 -- show lsp diagnostics in the signcolumn. See :help nvim_tree_lsp_diagnostics.
+g.nvim_tree_disable_window_picker = 1
 g.nvim_tree_auto_ignore_ft = { "dashboard" }
-g.nvim_tree_quit_on_open = 0
-g.nvim_tree_follow = 1
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_root_folder_modifier = ":~"
-g.nvim_tree_tab_open = 1
-g.nvim_tree_width_allow_resize = 0
-g.nvim_tree_disable_netrw = 0
-g.nvim_tree_hijack_netrw = 1
-g.nvim_tree_add_trailing = 0
-g.nvim_tree_group_empty = 1
-g.nvim_tree_lsp_diagnostics = 1
-g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
+g.nvim_tree_width_allow_resize = 1
+g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1 }
 
 g.nvim_tree_icons = {
     default = "",
@@ -83,6 +79,46 @@ g.nvim_tree_bindings = {
     { key = "[c", cb = tree_cb("prev_git_item") },
     { key = "]c", cb = tree_cb("next_git_item") },
     { key = "-", cb = tree_cb("dir_up") },
+    { key = "s", cb = tree_cb("system_open") },
     { key = "q", cb = tree_cb("close") },
     { key = "g?", cb = tree_cb("toggle_help") },
 }
+
+-- following options are the default
+require("nvim-tree").setup({
+    tree_follow = true,
+    -- disables netrw completely
+    disable_netrw = false,
+    -- hijack netrw window on startup
+    hijack_netrw = true,
+    -- open the tree when running this setup function
+    open_on_setup = false,
+    -- will not open on setup if the filetype is in this list
+    ignore_ft_on_setup = {},
+    -- closes neovim automatically when the tree is the last **WINDOW** in the view
+    auto_close = false,
+    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+    open_on_tab = false,
+    -- hijack the cursor in the tree to put it at the start of the filename
+    hijack_cursor = true,
+    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+    update_cwd = false,
+    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+    update_focused_file = {
+        -- enables the feature
+        enable = true,
+        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+        -- only relevant when `update_focused_file.enable` is true
+        update_cwd = false,
+        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+        ignore_list = {},
+    },
+    -- configuration options for the system open command (`s` in the tree by default)
+    system_open = {
+        -- the command to run this, leaving nil should work in most cases
+        cmd = nil,
+        -- the command arguments as a list
+        args = {},
+    },
+})
