66f023f711c58afc3d806edad8bfb7554d6e4eb5
landeaux <pluckedowl@gmail.com>
2021-09-30 22:54:40 -0700

    nvim: add dap config for javascript-node

--- nvim/lua/config/dap/configurations.lua
+++ nvim/lua/config/dap/configurations.lua
@@ -1,5 +1,23 @@
 local dap = require("dap")
 
+dap.adapters.node2 = {
+    type = "executable",
+    command = "node",
+    args = { vim.fn.stdpath("data") .. "/debuggers/vscode-node-debug2/out/src/nodeDebug.js" },
+}
+
+dap.configurations.javascript = {
+    {
+        type = "node2",
+        request = "launch",
+        program = "${workspaceFolder}/${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        console = "integratedTerminal",
+    },
+}
+
 dap.configurations.python = {
     {
         type = "python",
