54c66aaa6d2e309ec38d767dabcca7dd25535367
Adam Landis <pluckedowl@gmail.com>
2021-10-02 13:12:27 -0700

    nvim: add doc comments to reload_continue

--- nvim/lua/config/dap/utils.lua
+++ nvim/lua/config/dap/utils.lua
@@ -1,9 +1,13 @@
 local M = {}
 local dap = require("dap")
 
+-- Reload DAP configurations before starting a debug session
 function M.reload_continue()
+    -- invalidate the cache
     package.loaded["config.dap.configurations"] = nil
+    -- reload the configurations
     require("config.dap.configurations")
+    -- start the debugger
     dap.continue()
 end
 
