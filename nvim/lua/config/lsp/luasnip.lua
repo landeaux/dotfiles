23323bf3d33b07e360feb29632ad81c64d83e20f
Adam Landis <pluckedowl@gmail.com>
2021-10-08 09:43:16 -0700

    nvim: add store_selection_keys binding for luasnip

--- nvim/lua/config/lsp/luasnip.lua
+++ nvim/lua/config/lsp/luasnip.lua
@@ -1,3 +1,5 @@
+require("luasnip").config.setup({ store_selection_keys = "<CR>" })
+
 require("luasnip/loaders/from_vscode").lazy_load({
     paths = { "~/.local/share/nvim/site/pack/packer/start/friendly-snippets" },
 })
