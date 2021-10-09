aefa4390806aefecdb74cdff1bf4cc75512c5695
landeaux <pluckedowl@gmail.com>
2021-10-09 12:04:09 -0700

    nvim: add mapping to toggle colorizer

--- nvim/lua/config/utility/nvim-colorizer.lua
+++ nvim/lua/config/utility/nvim-colorizer.lua
@@ -1,1 +1,8 @@
 require("colorizer").setup()
+
+vim.api.nvim_set_keymap("n", "<Leader>tc", ":ColorizerToggle<CR>", { noremap = true })
+
+require("whichkey_setup").register_keymap(
+    "leader",
+    { t = { name = "+ui-toggle", c = "Colorizer" } }
+)
