3e29865223cd941db0cb9a8133454f009ca67ddb
Adam Landis <pluckedowl@gmail.com>
2021-10-13 09:49:02 -0700

    nvim: turn off snippet history for luasnip

--- nvim/lua/config/lsp/luasnip.lua
+++ nvim/lua/config/lsp/luasnip.lua
@@ -1,4 +1,4 @@
-require("luasnip").config.setup({ store_selection_keys = "<CR>" })
+require("luasnip").config.setup({ history = false, store_selection_keys = "<CR>" })
 
 require("luasnip/loaders/from_vscode").lazy_load({
     paths = { "~/.local/share/nvim/site/pack/packer/start/friendly-snippets" },
