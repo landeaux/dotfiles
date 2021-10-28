69e589f269ea7ae56828b6e1b84829cd82a64119
Adam Landis <pluckedowl@gmail.com>
2021-10-27 23:13:05 -0700

    nvim: add disabled module; refactor my/init.lua

--- nvim/lua/my/init.lua
+++ nvim/lua/my/init.lua
@@ -4,17 +4,18 @@ vim.g.mapleader = " "
 -- Set localleader to comma
 vim.g.maplocalleader = ","
 
--- Sensible defaults
-require("my.settings")
+local cosmic_modules = {
+    "my.disabled",
+    "my.settings",
+    "my.autocmd",
+    "my.plugins",
+    "my.keybinds",
+    "my.config",
+}
 
--- Autocmd
-require("my.autocmd")
-
--- Load plugins
-require("my.plugins")
-
--- Load keybinds
-require("my.keybinds")
-
--- Load configuration
-require("my.config")
+for _, mod in ipairs(cosmic_modules) do
+    local ok, err = pcall(require, mod)
+    if not ok then
+        error(("Error loading %s...\n\n%s"):format(mod, err))
+    end
+end
