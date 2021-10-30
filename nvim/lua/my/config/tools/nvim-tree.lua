8f0501bfeb34a31e59bdae9af75176e5443ecc6f
Adam Landis <pluckedowl@gmail.com>
2021-10-30 15:38:42 -0700

    nvim: update nvim-tree to address warnings

--- nvim/lua/my/config/tools/nvim-tree.lua
+++ nvim/lua/my/config/tools/nvim-tree.lua
@@ -2,11 +2,9 @@
 local g = vim.g
 local bind = vim.api.nvim_set_keymap
 
-g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
 g.nvim_tree_gitignore = 1
 g.nvim_tree_quit_on_open = 1 -- closes the tree when you open a file
 g.nvim_tree_indent_markers = 1 -- shows indent markers when folders are open
-g.nvim_tree_hide_dotfiles = 1 -- hides files and folders starting with a dot `.`
 g.nvim_tree_git_hl = 1 -- enable file highlight for git attributes (can be used without the icons)
 g.nvim_tree_highlight_opened_files = 1 -- enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_root_folder_modifier = ":~" -- see :help filename-modifiers for more options
@@ -43,6 +41,39 @@ local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
 -- following options are the default
 require("nvim-tree").setup({
+    auto_close = false,
+    diagnostics = { enable = true },
+    disable_netrw = false,
+    filters = {
+        dotfiles = true,
+        custom = { ".git", "node_modules", ".cache" },
+    },
+    hijack_cursor = true,
+    hijack_netrw = true,
+    ignore_ft_on_setup = {},
+    open_on_setup = false,
+    open_on_tab = false,
+    system_open = {
+        cmd = nil,
+        args = {},
+    },
+    tree_follow = true,
+    update_cwd = false,
+    update_focused_file = {
+        enable = true,
+        update_cwd = false,
+        ignore_list = {},
+    },
+    view = {
+        width = 45,
+        height = 30,
+        side = "left",
+        auto_resize = false,
+        mappings = {
+            custom_only = false,
+            list = {},
+        },
+    },
     bindings = {
         { key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb("edit") },
         { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
@@ -76,61 +107,6 @@ require("nvim-tree").setup({
         { key = "s", cb = tree_cb("system_open") },
         { key = "q", cb = tree_cb("close") },
         { key = "g?", cb = tree_cb("toggle_help") },
-    },
-    -- show lsp diagnostics in the signcolumn. See :help nvim_tree_lsp_diagnostics
-    diagnostics = { enable = true },
-    -- not sure what this does yet
-    tree_follow = true,
-    -- disables netrw completely
-    disable_netrw = false,
-    -- hijack netrw window on startup
-    hijack_netrw = true,
-    -- open the tree when running this setup function
-    open_on_setup = false,
-    -- will not open on setup if the filetype is in this list
-    ignore_ft_on_setup = {},
-    -- closes neovim automatically when the tree is the last **WINDOW** in the view
-    auto_close = false,
-    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-    open_on_tab = false,
-    -- hijack the cursor in the tree to put it at the start of the filename
-    hijack_cursor = true,
-    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-    update_cwd = false,
-    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-    update_focused_file = {
-        -- enables the feature
-        enable = true,
-        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-        -- only relevant when `update_focused_file.enable` is true
-        update_cwd = false,
-        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-        ignore_list = {},
-    },
-    -- configuration options for the system open command (`s` in the tree by default)
-    system_open = {
-        -- the command to run this, leaving nil should work in most cases
-        cmd = nil,
-        -- the command arguments as a list
-        args = {},
-    },
-    view = {
-        -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-        width = 45,
-        -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
-        height = 30,
-        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-        side = "left",
-        -- if true the tree will resize itself after opening a file
-        auto_resize = false,
-        mappings = {
-            -- custom only false will merge the list with the default mappings
-            -- if true, it will only use your list to set the mappings
-            custom_only = false,
-            -- list of mappings to set on the tree manually
-            list = {},
-        },
     },
 })
 
