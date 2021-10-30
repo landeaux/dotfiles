6e9e768d354d7cbd67c0fea7ba491d112e527538
Adam Landis <pluckedowl@gmail.com>
2021-10-30 15:20:41 -0700

    nvim: improve speed of telescope

--- nvim/lua/my/config/tools/telescope-nvim.lua
+++ nvim/lua/my/config/tools/telescope-nvim.lua
@@ -116,10 +116,19 @@ require("telescope").setup({
             },
         },
     },
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+    },
 })
 
--- Telescope modules
+-- Extensions
 require("telescope").load_extension("dap")
+require("telescope").load_extension("fzf")
 
 local bind_picker = require("my.config.tools.telescope-nvim-utils").bind_picker
 
