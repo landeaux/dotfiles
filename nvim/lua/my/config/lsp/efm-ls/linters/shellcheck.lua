1eed8328dc0c8eb7985c441d8a0af7f20c3d28f5
Adam Landis <pluckedowl@gmail.com>
2021-10-29 23:57:46 -0700

    nvim: fix lintCommand key name for shellcheck

--- nvim/lua/my/config/lsp/efm-ls/linters/shellcheck.lua
+++ nvim/lua/my/config/lsp/efm-ls/linters/shellcheck.lua
@@ -1,5 +1,5 @@
 local shellcheck = {
-    LintCommand = "shellcheck -f gcc -x",
+    lintCommand = "shellcheck -f gcc -x",
     lintFormats = { "%f:%l:%c: %trror: %m", "%f:%l:%c: %tarning: %m", "%f:%l:%c: %tote: %m" },
 }
 
