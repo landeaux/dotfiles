0e21f14a6c853d0fc9dcd277431fbb81beb24a3e
Adam Landis <pluckedowl@gmail.com>
2021-10-29 19:28:22 -0700

    nvim: remove BufRead event for gitsigns loading

--- nvim/lua/my/pluginsInit.lua
+++ nvim/lua/my/pluginsInit.lua
@@ -97,7 +97,6 @@ return packer.startup(function()
     use({
         "lewis6991/gitsigns.nvim",
         requires = { "nvim-lua/plenary.nvim" },
-        event = "BufRead",
         config = function()
             require("my.config.ui.gitsigns")
         end,
