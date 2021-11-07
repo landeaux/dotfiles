50960c5d1bfa98faf63a880e812238e68724e634
lando <pluckedowl@gmail.com>
2021-11-07 11:27:40 -0800

    format install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 rm_and_symlink() {
-  if [[ -e "$1" ]] || [[ -L "$1" ]]; then
-    rm -rf "$1"
-  fi
+	if [[ -e $1 ]] || [[ -L $1 ]]; then
+		rm -rf "$1"
+	fi
 	ln -s "$2" "$1"
 }
 export -f rm_and_symlink
