af884ba68e68b522e65120c15d2531212c90cf8c
lando <pluckedowl@gmail.com>
2021-11-07 14:18:52 -0800

    nvim: switch tokyonight_style to night

--- nvim/lua/my/config/ui/tokyonight.lua
+++ nvim/lua/my/config/ui/tokyonight.lua
@@ -1,5 +1,5 @@
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_style = "night"
 vim.g.tokyonight_sidebars = { "qf", "packer", "terminal" }
 vim.cmd("color tokyonight")
