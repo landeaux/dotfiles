4d93be4f76c583251c6b06197b0616956bb5142d
lando <pluckedowl@gmail.com>
2021-11-12 23:38:48 -0800

    nvim: improve diagnostics

--- nvim/lua/my/config/lsp/diagnostics.lua
+++ nvim/lua/my/config/lsp/diagnostics.lua
@@ -4,43 +4,21 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     {
         underline = true,
         virtual_text = {
+            source = "always",
             spacing = 4,
             prefix = "■", -- ﱢ
         },
         signs = {
-            -- Use a function to dynamically turn signs off
-            -- and on, using buffer local variables
-            enable = function(bufnr, client_id)
-                local ok, result = pcall(vim.api.nvim_buf_get_var, bufnr, "show_signs")
-                -- No buffer local variable set, so just enable by default
-                if not ok then
-                    return true
-                end
-
-                return result
-            end,
-
+            enable = true,
             priority = 20,
         },
-        -- Don't update in insert
         update_in_insert = false,
     }
 )
 
-vim.fn.sign_define(
-    "LspDiagnosticsSignError",
-    { texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError" }
-)
-vim.fn.sign_define(
-    "LspDiagnosticsSignWarning",
-    { texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning" }
-)
-vim.fn.sign_define(
-    "LspDiagnosticsSignHint",
-    { texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint" }
-)
-vim.fn.sign_define("LspDiagnosticsSignInformation", {
-    texthl = "LspDiagnosticsSignInformation",
-    text = "",
-    numhl = "LspDiagnosticsSignInformation",
-})
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
