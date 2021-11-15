0a471f1ffc8f4b7876e532e77af6b4d6dcc9f82e
landeaux <landeaux@users.noreply.github.com>
2021-11-14 18:46:01 -0800

    Update install_kitty.sh

--- scripts/install_kitty.sh
+++ scripts/install_kitty.sh
@@ -7,6 +7,7 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
 # your PATH)
+mkdir -p ~/.local/bin
 ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 # Place the kitty.desktop file somewhere it can be found by the OS
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
