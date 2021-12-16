51d31730018cce6003ad60879e07fa5572f3d78a
lando <pluckedowl@gmail.com>
2021-12-15 23:00:28 -0800

    nvim: add markdown parser for tree-sitter

--- nvim/lua/my/config/utility/tree-sitter.lua
+++ nvim/lua/my/config/utility/tree-sitter.lua
@@ -26,6 +26,7 @@ require("nvim-treesitter.configs").setup({
         "latex",
         "lua",
         "make",
+        "markdown",
         "php",
         "python",
         "r",
@@ -79,14 +80,14 @@ require("nvim-treesitter.configs").setup({
 })
 
 -- custom md
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.markdown = {
-    install_info = {
-        url = "https://github.com/ikatyang/tree-sitter-markdown",
-        files = { "src/parser.c", "src/scanner.cc" },
-    },
-    filetype = "md",
-}
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser_config.markdown = {
+--     install_info = {
+--         url = "https://github.com/ikatyang/tree-sitter-markdown",
+--         files = { "src/parser.c", "src/scanner.cc" },
+--     },
+--     filetype = "md",
+-- }
 
 local keymap_g = { name = "+goto", l = { name = "+ts-selection", i = "Initialize selection" } }
 
