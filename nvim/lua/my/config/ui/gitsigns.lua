178d61ab93eb3359d5a09c9a87c56883b76fc009
lando <pluckedowl@gmail.com>
2022-01-22 16:53:41 -0800

    nvim: move gitsigns mappings to on_attach

--- nvim/lua/my/config/ui/gitsigns.lua
+++ nvim/lua/my/config/ui/gitsigns.lua
@@ -1,4 +1,4 @@
-local bind = vim.api.nvim_set_keymap
+local bind = vim.api.nvim_buf_set_keymap
 
 require("gitsigns").setup({
     signs = {
@@ -43,62 +43,65 @@ require("gitsigns").setup({
     diff_opts = {
         internal = true,
     },
-})
+    on_attach = function(bufnr)
+        -- Default keymap options
+        local opts = { noremap = true }
 
--- Default keymap options
-local opts = { noremap = true }
+        bind(
+            bufnr,
+            "n",
+            "]h",
+            "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'",
+            { noremap = true, expr = true }
+        )
+        bind(
+            bufnr,
+            "n",
+            "[h",
+            "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'",
+            { noremap = true, expr = true }
+        )
 
-bind(
-    "n",
-    "]h",
-    "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'",
-    { noremap = true, expr = true }
-)
-bind(
-    "n",
-    "[h",
-    "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'",
-    { noremap = true, expr = true }
-)
+        bind(bufnr, "n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line({full = true})<CR>', opts)
+        bind(bufnr, "n", "<leader>hp", '<cmd>lua require"gitsigns".preview_hunk()<CR>', opts)
+        bind(bufnr, "n", "<leader>hr", '<cmd>lua require"gitsigns".reset_hunk()<CR>', opts)
+        bind(bufnr, "v", "<leader>hr", '<cmd>lua require"gitsigns".reset_hunk()<CR>', opts)
+        bind(bufnr, "n", "<leader>hR", '<cmd>lua require"gitsigns".reset_buffer()<CR>', opts)
+        bind(bufnr, "n", "<leader>hs", '<cmd>lua require"gitsigns".stage_hunk()<CR>', opts)
+        bind(bufnr, "v", "<leader>hs", '<cmd>lua require"gitsigns".stage_hunk()<CR>', opts)
+        bind(bufnr, "n", "<leader>hS", '<cmd>lua require"gitsigns".stage_buffer()<CR>', opts)
+        bind(bufnr, "n", "<leader>hu", '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>', opts)
+        bind(bufnr, "n", "<leader>tb", '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>', opts)
+        bind(bufnr, "n", "<leader>td", '<cmd>lua require"gitsigns".toggle_deleted()<CR>', opts)
 
-bind("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line({full = true})<CR>', opts)
-bind("n", "<leader>hp", '<cmd>lua require"gitsigns".preview_hunk()<CR>', opts)
-bind("n", "<leader>hr", '<cmd>lua require"gitsigns".reset_hunk()<CR>', opts)
-bind("v", "<leader>hr", '<cmd>lua require"gitsigns".reset_hunk()<CR>', opts)
-bind("n", "<leader>hR", '<cmd>lua require"gitsigns".reset_buffer()<CR>', opts)
-bind("n", "<leader>hs", '<cmd>lua require"gitsigns".stage_hunk()<CR>', opts)
-bind("v", "<leader>hs", '<cmd>lua require"gitsigns".stage_hunk()<CR>', opts)
-bind("n", "<leader>hS", '<cmd>lua require"gitsigns".stage_buffer()<CR>', opts)
-bind("n", "<leader>hu", '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>', opts)
-bind("n", "<leader>tb", '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>', opts)
-bind("n", "<leader>td", '<cmd>lua require"gitsigns".toggle_deleted()<CR>', opts)
+        bind(bufnr, "o", "ih", "<cmd>Gitsigns select_hunk<CR>", opts)
+        bind(bufnr, "x", "ih", "<cmd>Gitsigns select_hunk<CR>", opts)
 
-bind("o", "ih", '<cmd>Gitsigns select_hunk<CR>', opts)
-bind("x", "ih", '<cmd>Gitsigns select_hunk<CR>', opts)
+        local wk = require("whichkey_setup")
 
-local wk = require("whichkey_setup")
+        wk.register_keymap("leader", {
+            h = {
+                name = "+hunk",
+                b = "Blame line",
+                p = "Preview hunk",
+                r = "Reset hunk",
+                R = "Reset buffer",
+                s = "Stage hunk",
+                S = "Stage buffer",
+                u = "Undo stage hunk",
+            },
+            t = {
+                b = "Current line blame",
+                d = "Deleted hunks",
+            },
+        })
 
-wk.register_keymap("leader", {
-    h = {
-        name = "+hunk",
-        b = "Blame line",
-        p = "Preview hunk",
-        r = "Reset hunk",
-        R = "Reset buffer",
-        s = "Stage hunk",
-        S = "Stage buffer",
-        u = "Undo stage hunk",
-    },
-    t = {
-        b = "Current line blame",
-        d = "Deleted hunks",
-    },
-})
-
-wk.register_keymap("visual", {
-    h = {
-        name = "+hunk",
-        r = "Reset hunk",
-        s = "Stage hunk",
-    },
+        wk.register_keymap("visual", {
+            h = {
+                name = "+hunk",
+                r = "Reset hunk",
+                s = "Stage hunk",
+            },
+        })
+    end,
 })
