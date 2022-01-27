047c210a3bfe2d7a25011384b093a8119827730c
lando <pluckedowl@gmail.com>
2022-01-27 00:03:38 -0800

    nvim: fix volar lsp config

--- nvim/lua/my/config/lsp/providers/init.lua
+++ nvim/lua/my/config/lsp/providers/init.lua
@@ -1,5 +1,8 @@
 require("my.config.lsp.providers.volar")
 
+-- vim.lsp.set_log_level("trace")
+-- require("vim.lsp.log").set_format_func(vim.inspect)
+
 local default_config = require("my.config.lsp.providers.defaults")
 local lsp_installer = require("nvim-lsp-installer")
 
@@ -31,7 +34,7 @@ local requested_servers = {
     "intelliphense",
     "pyright",
     "vimls",
-    "volar", -- NOTE: This can't exist alongside vuels
+    -- "volar", -- NOTE: This can't exist alongside vuels
     -- "vuels",
     "yamlls",
 }
@@ -48,22 +51,30 @@ for _, requested_server in pairs(requested_servers) do
 end
 
 lsp_installer.on_server_ready(function(server)
+    if server.name == "volar" then
+        return
+    end
+
     local opts = default_config
 
     opts.autostart = true
 
-    if vim.startswith(server.name, "volar") then
-        server:setup(opts)
-    else
-        local custom_config_path = "my.config.lsp.providers." .. server.name
-        local custom_config_exists, config = pcall(require, custom_config_path)
-        if custom_config_exists then
-            opts = vim.tbl_deep_extend("force", opts, config)
-        end
-
-        -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
-        server:setup(opts)
+    local custom_config_path = "my.config.lsp.providers." .. server.name
+    local custom_config_exists, config = pcall(require, custom_config_path)
+    if custom_config_exists then
+        opts = vim.tbl_deep_extend("force", opts, config)
     end
+
+    -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
+    server:setup(opts)
 
     vim.cmd([[ do User LspAttachBuffers ]])
 end)
+
+require("my.config.lsp.providers.volar").register_volar_lspconfigs()
+
+local lspconfig = require("lspconfig")
+
+lspconfig.volar_api.setup(default_config)
+lspconfig.volar_doc.setup(default_config)
+lspconfig.volar_html.setup(default_config)
