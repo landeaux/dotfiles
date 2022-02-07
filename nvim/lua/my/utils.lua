404909705aafa3c238dfc277637c3a0a7e8cb8c3
lando <pluckedowl@gmail.com>
2022-02-06 17:08:21 -0800

    nvim: remove unused utils

--- nvim/lua/my/utils.lua
+++ nvim/lua/my/utils.lua
@@ -1,51 +1,9 @@
 local M = {}
-local cmd = vim.cmd
-local types = { o = vim.o, b = vim.bo, w = vim.wo }
-local buf_bind = vim.api.nvim_buf_set_keymap
-
--- Get table length
-function M.length(table)
-    local count = 0
-    for _ in ipairs(table) do
-        count = count + 1
-    end
-    return count
-end
-
--- Get option
-function M.get_opt(type, name)
-    return types[type][name]
-end
-
--- Set option
-function M.set_opt(type, name, value)
-    types[type][name] = value
-
-    if type ~= "o" then
-        types["o"][name] = value
-    end
-end
-
--- Append option to a list of options
-function M.append_opt(type, name, value)
-    local current_value = M.get_opt(type, name)
-
-    if not string.match(current_value, value) then
-        M.set_opt(type, name, current_value .. value)
-    end
-end
-
--- Remove option from a list of options
-function M.remove_opt(type, name, value)
-    local current_value = M.get_opt(type, name)
-
-    if string.match(current_value, value) then
-        M.set_opt(type, name, string.gsub(current_value, value, ""))
-    end
-end
 
 -- Create an augroup
 function M.create_augroup(autocmds, name)
+    local cmd = vim.cmd
+
     cmd("augroup " .. name)
     cmd("autocmd!")
 
@@ -59,6 +17,7 @@ end
 -- Make navigation keys navigate through display lines instead of physical lines
 function M.set_buffer_soft_line_nagivation()
     local opts = { noremap = true, silent = true }
+    local buf_bind = vim.api.nvim_buf_set_keymap
 
     buf_bind(0, "n", "k", "gk", opts)
     buf_bind(0, "n", "j", "gj", opts)
@@ -86,29 +45,6 @@ function M.set_buffer_soft_line_nagivation()
     buf_bind(0, "i", "<Down>", "<C-o>gj", opts)
     buf_bind(0, "i", "<Home>", "<C-o>g<Home>", opts)
     buf_bind(0, "i", "<End>", "<C-o>g<End>", opts)
-end
-
--- escape terminal codes and keycodes
-function M.t(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
--- gets the highlight value for the given group and term
-function M.get_hi_term(group, term)
-    local output = vim.api.nvim_exec("hi " .. group, true)
-    return vim.fn.matchstr(output, term .. "=\\zs\\S*")
-end
-
-function M.has_neovim_v05()
-    if vim.fn.has("nvim-0.5") == 1 then
-        return true
-    end
-    return false
-end
-
-function M.is_root()
-    local output = vim.fn.systemlist("id -u")
-    return ((output[1] or "") == "0")
 end
 
 return M
