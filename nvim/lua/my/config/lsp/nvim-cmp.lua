35ecb32756768b3cdef2b227d36bc128411c4983
Adam Landis <pluckedowl@gmail.com>
2022-03-04 10:04:06 -0800

    nvim: don't auto-complete on <Tab> when in snippet

--- nvim/lua/my/config/lsp/nvim-cmp.lua
+++ nvim/lua/my/config/lsp/nvim-cmp.lua
@@ -3,9 +3,9 @@ local luasnip = require("luasnip")
 
 local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0
-        and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
-            == nil
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+        :sub(col, col)
+        :match("%s") == nil
 end
 
 cmp.setup({
@@ -36,14 +36,16 @@ cmp.setup({
             select = true,
         }),
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
+            if luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
+            elseif cmp.visible() then
+                cmp.select_next_item()
             elseif has_words_before() then
                 cmp.complete()
             else
-                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                -- The fallback function sends an already mapped key. In this
+                -- case, it's probably `<Tab>`.
+                fallback()
             end
         end, {
             "i",
