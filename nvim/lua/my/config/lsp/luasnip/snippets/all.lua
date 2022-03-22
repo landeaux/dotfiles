1173f973a1dedd2bc532b148be1606250ebc1d29
Adam Landis <pluckedowl@gmail.com>
2022-03-22 16:39:33 -0700

    nvim: remove ":" in snippet keywords

--- nvim/lua/my/config/lsp/luasnip/snippets/all.lua
+++ nvim/lua/my/config/lsp/luasnip/snippets/all.lua
@@ -8,11 +8,11 @@ local my_utils = require("my.config.lsp.luasnip.utils")
 
 return {
     -- Use a function to execute any shell command and print its text.
-    s("sh:tree", f(my_utils.bash, {}, { user_args = { "tree -L 2" } })),
+    s("shtree", f(my_utils.bash, {}, { user_args = { "tree -L 2" } })),
 
     -- Directly insert the output from a function evaluated at runtime.
-    s("dt:y", p(os.date, "%Y")),
-    s("dt:ym", p(os.date, "%Y-%M")),
-    s("dt:ymd", p(os.date, "%Y-%M-%d")),
-    s("dt:ymdt", p(os.date, "%Y-%M-%dT%H:%M:%S")),
+    s("dty", p(os.date, "%Y")),
+    s("dtym", p(os.date, "%Y-%M")),
+    s("dtymd", p(os.date, "%Y-%M-%d")),
+    s("dtymdt", p(os.date, "%Y-%M-%dT%H:%M:%S")),
 }
