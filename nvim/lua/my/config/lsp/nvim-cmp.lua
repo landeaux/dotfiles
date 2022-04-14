5dd08c90425921b82aedc6f8b263b64fefb0c307
lando <pluckedowl@gmail.com>
2022-04-13 22:59:30 -0700

    nvim: restore previous cmp mappings

--- nvim/lua/my/config/lsp/nvim-cmp.lua
+++ nvim/lua/my/config/lsp/nvim-cmp.lua
@@ -1,49 +1,50 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
-local select_next_cmp_item = function(fallback)
-    if cmp.visible() then
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0
+        and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
+            == nil
+end
+
+local select_next_snippet_choice_or_cmp_item = function(fallback)
+    if luasnip.choice_active() then
+        luasnip.change_choice(1)
+    elseif cmp.visible() then
         cmp.select_next_item()
     else
         fallback()
     end
 end
 
-local select_prev_cmp_item = function(fallback)
-    if cmp.visible() then
+local select_prev_snippet_choice_or_cmp_item = function(fallback)
+    if luasnip.choice_active() then
+        luasnip.change_choice(-1)
+    elseif cmp.visible() then
         cmp.select_prev_item()
     else
         fallback()
     end
 end
 
-local jump_to_next_snippet_node = function(fallback)
-    if luasnip.jumpable() then
-        luasnip.jump()
+local expand_jump_or_select_next_cmp_item = function(fallback)
+    if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+    elseif cmp.visible() then
+        cmp.select_next_item()
+    elseif has_words_before() then
+        cmp.complete()
     else
         fallback()
     end
 end
 
-local jump_to_prev_snippet_node = function(fallback)
+local jump_or_select_prev_cmp_item = function(fallback)
     if luasnip.jumpable(-1) then
         luasnip.jump(-1)
-    else
-        fallback()
-    end
-end
-
-local select_next_snippet_choice = function(fallback)
-    if luasnip.choice_active() then
-        luasnip.change_choice(1)
-    else
-        fallback()
-    end
-end
-
-local select_prev_snippet_choice = function(fallback)
-    if luasnip.choice_active() then
-        luasnip.change_choice(-1)
+    elseif cmp.visible() then
+        cmp.select_prev_item()
     else
         fallback()
     end
@@ -61,19 +62,19 @@ cmp.setup({
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        -- General cmp mappings
-        ["<C-n>"] = cmp.mapping(select_next_cmp_item, { "i", "s" }),
-        ["<C-p>"] = cmp.mapping(select_prev_cmp_item, { "i", "s" }),
+        ["<C-n>"] = cmp.mapping(select_next_snippet_choice_or_cmp_item, { "i", "s" }),
+        ["<C-j>"] = cmp.mapping(select_next_snippet_choice_or_cmp_item, { "i", "s" }),
+        ["<C-p>"] = cmp.mapping(select_prev_snippet_choice_or_cmp_item, { "i", "s" }),
+        ["<C-k>"] = cmp.mapping(select_prev_snippet_choice_or_cmp_item, { "i", "s" }),
         ["<C-d>"] = cmp.mapping.scroll_docs(-5),
         ["<C-f>"] = cmp.mapping.scroll_docs(5),
         ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
         ["<C-e>"] = cmp.mapping.close(),
         ["<C-c>"] = cmp.mapping.abort(),
-        -- Snippet mappings
-        ["<C-l>"] = cmp.mapping(jump_to_next_snippet_node, { "i", "s" }),
-        ["<C-h>"] = cmp.mapping(jump_to_prev_snippet_node, { "i", "s" }),
-        ["<C-j>"] = cmp.mapping(select_next_snippet_choice, { "i", "s" }),
-        ["<C-k>"] = cmp.mapping(select_prev_snippet_choice, { "i", "s" }),
+        ["<Tab>"] = cmp.mapping(expand_jump_or_select_next_cmp_item, { "i", "s" }),
+        ["<C-l>"] = cmp.mapping(expand_jump_or_select_next_cmp_item, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(jump_or_select_prev_cmp_item, { "i", "s" }),
+        ["<C-h>"] = cmp.mapping(jump_or_select_prev_cmp_item, { "i", "s" }),
     }),
     sources = {
         -- NOTE: The order of these are important as it determines priority.
