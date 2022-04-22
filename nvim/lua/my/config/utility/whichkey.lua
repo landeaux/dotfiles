2740da5af1a40a2999dd5c6e7ec965e6203e71d0
lando <pluckedowl@gmail.com>
2022-04-21 23:27:07 -0700

    nvim: update whichkey config
    - disable registers (breaks in telescope)
    - enable spelling

--- nvim/lua/my/config/utility/whichkey.lua
+++ nvim/lua/my/config/utility/whichkey.lua
@@ -1,3 +1,10 @@
-require("which-key").setup({})
+require("which-key").setup({
+    plugins = {
+        registers = false,
+        spelling = {
+            enabled = true,
+        },
+    },
+})
 
 vim.o.timeoutlen = 500 -- By default timeoutlen is 1000 ms
