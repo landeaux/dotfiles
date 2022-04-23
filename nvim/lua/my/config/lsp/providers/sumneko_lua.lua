8982c11b92eb90b06f0947b7a2239807370364e3
lando <pluckedowl@gmail.com>
2022-04-23 12:12:39 -0700

    nvim(sumneko_lua): extend runtime path and workspace lib
    for better completions and diagnostics

--- nvim/lua/my/config/lsp/providers/sumneko_lua.lua
+++ nvim/lua/my/config/lsp/providers/sumneko_lua.lua
@@ -1,4 +1,8 @@
 -- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
 return {
     settings = {
         Lua = {
@@ -6,7 +10,7 @@ return {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = "LuaJIT",
                 -- Setup your lua path
-                path = vim.split(package.path, ";"),
+                path = runtime_path,
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
@@ -14,11 +18,16 @@ return {
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                },
+                library = vim.api.nvim_get_runtime_file("", true),
+                -- library = {
+                --     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                --     [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                -- },
                 maxPreload = 10000,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
             },
         },
     },
