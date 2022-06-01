0bf625d0433a895d209b8b408f93feead891d00a
Adam Landis <pluckedowl@gmail.com>
2022-06-01 09:51:40 -0700

    nvim: restore bufferline offsets option

--- nvim/lua/my/config/ui/nvim-bufferline.lua
+++ nvim/lua/my/config/ui/nvim-bufferline.lua
@@ -15,7 +15,7 @@ require("bufferline").setup({
                 return true
             end
         end,
-        -- offsets = { { filetype = "neo-tree", text = "File Explorer", text_align = "left" } },
+        offsets = { { filetype = "neo-tree", text = "File Explorer", text_align = "left" } },
         show_buffer_close_icons = false,
         show_close_icon = false,
         separator_style = "thin", -- "slant" | "thick" | "thin" | { 'any', 'any' },
