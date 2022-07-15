2059ec7c288252255fc7991a4cecc16a859d2b98
Adam Landis <pluckedowl@gmail.com>
2022-07-15 10:07:37 -0700

    nvim(dap): remove commented out imports in dap init

--- nvim/lua/my/config/dap/init.lua
+++ nvim/lua/my/config/dap/init.lua
@@ -7,9 +7,3 @@ require("my.config.dap.mapping")
 -- Load DAP adapters and configurations
 require("my.config.dap.adapters")
 require("my.config.dap.configurations")
-
--- Load DAP language-specific configs
--- require("my.config.dap.python")
-
--- Load DAP UI settings
--- require("my.config.dap.ui")
