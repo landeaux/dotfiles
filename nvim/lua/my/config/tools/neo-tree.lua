ba6254017167d769df556c58ca427026b30643b7
lando <pluckedowl@gmail.com>
2022-07-30 14:17:19 -0700

    nvim: update neo-tree config

--- nvim/lua/my/config/tools/neo-tree.lua
+++ nvim/lua/my/config/tools/neo-tree.lua
@@ -32,7 +32,19 @@ require("neo-tree").setup({
     popup_border_style = "rounded",
     enable_git_status = true,
     enable_diagnostics = true,
+    sort_case_insensitive = false, -- used when sorting files and directories in the tree
+    sort_function = nil, -- use a custom function for sorting files and directories in the tree
+    -- sort_function = function (a,b)
+    --       if a.type == b.type then
+    --           return a.path > b.path
+    --       else
+    --           return a.type > b.type
+    --       end
+    --   end , -- this sorts files and directories descendantly
     default_component_configs = {
+        container = {
+            enable_character_fade = true,
+        },
         indent = {
             indent_size = 2,
             padding = 1, -- extra padding on left hand side
@@ -51,7 +63,10 @@ require("neo-tree").setup({
             folder_closed = "",
             folder_open = "",
             folder_empty = "ﰊ",
+            -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+            -- then these will never be used.
             default = "*",
+            highlight = "NeoTreeFileIcon",
         },
         modified = {
             symbol = "[+]",
@@ -60,13 +75,14 @@ require("neo-tree").setup({
         name = {
             trailing_slash = false,
             use_git_status_colors = true,
+            highlight = "NeoTreeFileName",
         },
         git_status = {
             symbols = {
                 -- Change type
                 added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
                 modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-                deleted = "", -- this can only be used in the git_status source
+                deleted = "✖", -- this can only be used in the git_status source
                 renamed = "", -- this can only be used in the git_status source
                 -- Status type
                 untracked = "",
@@ -80,52 +96,81 @@ require("neo-tree").setup({
     window = {
         position = "left",
         width = 40,
+        mapping_options = {
+            noremap = true,
+            nowait = true,
+        },
         mappings = {
-            ["t"] = "toggle_node",
+            ["<space>"] = {
+                "toggle_node",
+                nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+            },
             ["<2-LeftMouse>"] = "open",
             ["<cr>"] = "open",
             ["S"] = "open_split",
-            ["v"] = "open_vsplit",
-            ["T"] = "open_tabnew",
+            ["s"] = "open_vsplit",
+            -- ["S"] = "split_with_window_picker",
+            -- ["s"] = "vsplit_with_window_picker",
+            ["t"] = "open_tabnew",
+            ["w"] = "open_with_window_picker",
             ["C"] = "close_node",
-            ["a"] = "add",
-            ["A"] = "add_directory",
+            ["z"] = "close_all_nodes",
+            --["Z"] = "expand_all_nodes",
+            ["a"] = {
+                "add",
+                -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                config = {
+                    show_path = "none", -- "none", "relative", "absolute"
+                },
+            },
+            ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add".
             ["d"] = "delete",
             ["r"] = "rename",
             ["y"] = "copy_to_clipboard",
             ["x"] = "cut_to_clipboard",
             ["p"] = "paste_from_clipboard",
-            ["c"] = "copy", -- takes text input for destination
-            ["m"] = "move", -- takes text input for destination
+            ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+            -- ["c"] = {
+            --  "copy",
+            --  config = {
+            --    show_path = "none" -- "none", "relative", "absolute"
+            --  }
+            --}
+            ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
             ["q"] = "close_window",
             ["R"] = "refresh",
+            ["?"] = "show_help",
+            ["<"] = "prev_source",
+            [">"] = "next_source",
         },
     },
     nesting_rules = {},
     filesystem = {
-        async_directory_scan = false,
         filtered_items = {
             visible = false, -- when true, they will just be displayed differently than normal items
             hide_dotfiles = false,
             hide_gitignored = true,
+            hide_hidden = true, -- only works on Windows for hidden files/directories
             hide_by_name = {
-                ".DS_Store",
-                "thumbs.db",
                 --"node_modules"
             },
+            hide_by_pattern = { -- uses glob style patterns
+                --"*.meta"
+            },
             never_show = { -- remains hidden even if visible is toggled to true
-                --".DS_Store",
+                -- ".DS_Store",
                 --"thumbs.db"
             },
         },
         follow_current_file = true, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
+        group_empty_dirs = false, -- when true, empty folders will be grouped together
         hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
         -- in whatever position is specified in window.position
         -- "open_current",  -- netrw disabled, opening a directory opens within the
         -- window like netrw would, regardless of window.position
         -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-        use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+        use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
         -- instead of relying on nvim autocmd events.
         window = {
             mappings = {
@@ -134,14 +179,20 @@ require("neo-tree").setup({
                 ["H"] = "toggle_hidden",
                 ["I"] = "toggle_gitignore",
                 ["/"] = "fuzzy_finder",
+                ["D"] = "fuzzy_finder_directory",
                 ["F"] = "filter_on_submit",
                 ["ff"] = find_files,
                 ["fg"] = live_grep,
                 ["<c-x>"] = "clear_filter",
+                ["[g"] = "prev_git_modified",
+                ["]g"] = "next_git_modified",
             },
         },
     },
     buffers = {
+        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        -- time the current file is changed while the tree is open.
+        group_empty_dirs = true, -- when true, empty folders will be grouped together
         show_unloaded = true,
         window = {
             mappings = {
--- nvim/lua/my/pluginsInit.lua
+++ nvim/lua/my/pluginsInit.lua
@@ -134,7 +134,30 @@ packer.startup(function()
         branch = "v2.x",
         requires = {
             "nvim-lua/plenary.nvim",
+            "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
+            {
+                -- only needed if you want to use the commands with "_with_window_picker" suffix
+                "s1n7ax/nvim-window-picker",
+                tag = "v1.*",
+                config = function()
+                    require("window-picker").setup({
+                        autoselect_one = true,
+                        include_current = false,
+                        filter_rules = {
+                            -- filter using buffer options
+                            bo = {
+                                -- if the file type is one of following, the window will be ignored
+                                filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
+
+                                -- if the buffer type is one of following, the window will be ignored
+                                buftype = { "terminal" },
+                            },
+                        },
+                        other_win_hl_color = "#e35e4f",
+                    })
+                end,
+            },
         },
         config = function()
             require("my.config.tools.neo-tree")
@@ -143,7 +166,7 @@ packer.startup(function()
             "Neotree",
         },
         keys = "<Leader>tn",
-        after = { "nvim-web-devicons", "which-key.nvim" },
+        after = { "which-key.nvim" },
     })
 
     -- Git
