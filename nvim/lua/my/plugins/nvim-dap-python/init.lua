c6139a0f85b72f0aa60810488a55ef79ea48b223
lando <pluckedowl@gmail.com>
2022-09-05 13:04:20 -0700

    nvim: stabilize nvim-dap-python setup

--- nvim/lua/my/plugins/nvim-dap-python/init.lua
+++ nvim/lua/my/plugins/nvim-dap-python/init.lua
@@ -1,5 +1,10 @@
 local dap_python = require("dap-python")
 
-local python_path = vim.fn.getenv("PYENV_ROOT") .. "/versions/debugpy/bin/python"
+local pyenv_root = vim.fn.getenv("PYENV_ROOT")
+if pyenv_root == vim.NIL then
+    return
+end
+
+local python_path = pyenv_root .. "/versions/debugpy/bin/python"
 dap_python.setup(python_path)
 dap_python.test_runner = "pytest"
