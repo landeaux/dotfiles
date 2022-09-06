35de1222e61eaf3f5f8ce2b63591dcfc80facb58
lando <pluckedowl@gmail.com>
2022-09-06 10:03:25 -0700

    nvim: utilize map_factory helper

--- nvim/lua/my/utils.lua
+++ nvim/lua/my/utils.lua
@@ -10,28 +10,6 @@ function M.create_augroup(autocmds, group_name)
     end
 end
 
--- Make navigation keys navigate through display lines instead of physical lines
-function M.set_buffer_soft_line_nagivation()
-    local map = vim.keymap.set
-    local opts = { buffer = true, silent = true }
-
-    map({ "n", "o" }, "$", "g$", opts)
-    map({ "n", "o" }, "0", "g0", opts)
-    map({ "n", "o" }, "^", "g^", opts)
-    map({ "n", "o" }, "j", "gj", opts)
-    map({ "n", "o" }, "k", "gk", opts)
-
-    map({ "n", "o" }, "<Down>", "gj", opts)
-    map({ "n", "o" }, "<End>", "g<End>", opts)
-    map({ "n", "o" }, "<Home>", "g<Home>", opts)
-    map({ "n", "o" }, "<Up>", "gk", opts)
-
-    map("i", "<Down>", "<C-o>gj", opts)
-    map("i", "<End>", "<C-o>g<End>", opts)
-    map("i", "<Home>", "<C-o>g<Home>", opts)
-    map("i", "<Up>", "<C-o>gk", opts)
-end
-
 function M.map_factory(default_opts)
     default_opts = default_opts or {}
     local _map = function(mode, l, r, opts)
@@ -40,6 +18,27 @@ function M.map_factory(default_opts)
         vim.keymap.set(mode, l, r, merged_opts)
     end
     return _map
+end
+
+-- Make navigation keys navigate through display lines instead of physical lines
+function M.set_buffer_soft_line_nagivation()
+    local map = M.map_factory({ buffer = true, silent = true })
+
+    map({ "n", "o" }, "$", "g$")
+    map({ "n", "o" }, "0", "g0")
+    map({ "n", "o" }, "^", "g^")
+    map({ "n", "o" }, "j", "gj")
+    map({ "n", "o" }, "k", "gk")
+
+    map({ "n", "o" }, "<Down>", "gj")
+    map({ "n", "o" }, "<End>", "g<End>")
+    map({ "n", "o" }, "<Home>", "g<Home>")
+    map({ "n", "o" }, "<Up>", "gk")
+
+    map("i", "<Down>", "<C-o>gj")
+    map("i", "<End>", "<C-o>g<End>")
+    map("i", "<Home>", "<C-o>g<Home>")
+    map("i", "<Up>", "<C-o>gk")
 end
 
 return M
