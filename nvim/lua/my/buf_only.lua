5c014590449948c052c978deb676b0d0f773afd3
Adam Landis <pluckedowl@gmail.com>
2022-09-07 15:18:19 -0700

    nvim: make BufOnly mapping silent

--- nvim/lua/my/buf_only.lua
+++ nvim/lua/my/buf_only.lua
@@ -6,13 +6,15 @@ end
 
 vim.api.nvim_create_user_command("BufOnly", function()
     M.buf_only()
-end, { desc = "Close all buffers except the current one" })
+end, {
+    desc = "Close all buffers except the current one",
+})
 
 vim.keymap.set(
     "n",
     "<Leader>bo",
     ":BufOnly<CR>",
-    { desc = "Close all buffers except the current one" }
+    { desc = "Close all buffers except the current one", silent = true }
 )
 
 return M
