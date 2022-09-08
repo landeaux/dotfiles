2e8963bf7ecb4a32fb680d5673aa2af2b5d1e7a8
lando <pluckedowl@gmail.com>
2022-09-07 23:35:52 -0700

    revert BufOnly changes...

--- nvim/lua/my/buf_only.lua
+++ nvim/lua/my/buf_only.lua
@@ -1,26 +1,26 @@
 local M = {}
 
--- M.buf_only = function()
---     vim.fn.execute("%bd|e#|bd#")
--- end
-
-local buf_opt = vim.api.nvim_buf_get_option
-
-function M.buf_only()
-    local cur = vim.api.nvim_get_current_buf()
-
-    for _, n in ipairs(vim.api.nvim_list_bufs()) do
-        if
-            n == cur
-            or buf_opt(n, "modified")
-            or buf_opt(n, "buftype") == "terminal"
-            or not buf_opt(n, "modifiable")
-        then
-            return
-        end
-        vim.api.nvim_buf_delete(n, {})
-    end
+M.buf_only = function()
+    vim.fn.execute("%bd|e#|bd#")
 end
+
+-- local buf_opt = vim.api.nvim_buf_get_option
+
+-- function M.buf_only()
+--     local cur = vim.api.nvim_get_current_buf()
+
+--     for _, n in ipairs(vim.api.nvim_list_bufs()) do
+--         if
+--             n == cur
+--             or buf_opt(n, "modified")
+--             or buf_opt(n, "buftype") == "terminal"
+--             or not buf_opt(n, "modifiable")
+--         then
+--             return
+--         end
+--         vim.api.nvim_buf_delete(n, {})
+--     end
+-- end
 
 vim.api.nvim_create_user_command("BufOnly", function()
     M.buf_only()
