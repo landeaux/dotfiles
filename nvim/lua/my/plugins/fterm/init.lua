cc2b7369fb4b32ee4d9621b333c85391f8670a28
lando <pluckedowl@gmail.com>
2022-09-12 09:18:00 -0700

    nvim: add Node command to open node REPL via FTerm

--- nvim/lua/my/plugins/fterm/init.lua
+++ nvim/lua/my/plugins/fterm/init.lua
@@ -9,3 +9,7 @@ require("FTerm").setup({
 vim.api.nvim_create_user_command("Python", function()
     require("FTerm").run("pipenv run python")
 end, { bang = true })
+
+vim.api.nvim_create_user_command("Node", function()
+    require("FTerm").run("node")
+end, { bang = true })
