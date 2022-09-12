71f494dd406fb93b83ab7aba7999df99ac82effc
Adam Landis <pluckedowl@gmail.com>
2022-09-12 10:16:07 -0700

    nvim: fix <M-p> binding for invoking python shell

--- nvim/lua/my/plugins/fterm/mappings.lua
+++ nvim/lua/my/plugins/fterm/mappings.lua
@@ -5,6 +5,4 @@ map("n", "<A-i>", function()
 end, { desc = "Toggle terminal" })
 map("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { desc = "Toggle terminal" })
 
-map("n", "<A-p>", function()
-    require("FTerm").toggle()
-end, { desc = "Invoke python shell (via pipenv)" })
+map("n", "<A-p>", ":Python<CR>", { desc = "Invoke python shell (via pipenv)" })
