7e5a2a598c427e20efca451ff077d0f95dd7342f
lando <pluckedowl@gmail.com>
2022-10-15 10:22:01 -0700

    nvim: address cmp_nvim_lsp breaking change for setting capabilities

--- nvim/lua/my/plugins/lsp/providers/defaults.lua
+++ nvim/lua/my/plugins/lsp/providers/defaults.lua
@@ -74,8 +74,7 @@ M.flags = {
     debounce_text_changes = 150,
 }
 
-M.capabilities =
-    require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+M.capabilities = require("cmp_nvim_lsp").default_capabilities()
 M.capabilities.textDocument.colorProvider = {
     dynamicRegistration = true,
 }
