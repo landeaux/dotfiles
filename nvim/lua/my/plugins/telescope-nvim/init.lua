28a545b6c954e01e96e85667448216d52eca2fb8
lando <pluckedowl@gmail.com>
2022-12-05 09:26:01 -0800

    nvim(telescope): try previewer.filesize_limit again

--- nvim/lua/my/plugins/telescope-nvim/init.lua
+++ nvim/lua/my/plugins/telescope-nvim/init.lua
@@ -11,29 +11,32 @@ local set_prompt_to_entry_value = function(prompt_bufnr)
     action_state.get_current_picker(prompt_bufnr):reset_prompt(entry.ordinal)
 end
 
-local previewers = require("telescope.previewers")
-local new_maker = function(filepath, bufnr, opts)
-    opts = opts or {}
-
-    filepath = vim.fn.expand(filepath)
-    vim.loop.fs_stat(filepath, function(_, stat)
-        if not stat then
-            return
-        end
-        if stat.size > 100000 then
-            return
-        else
-            previewers.buffer_previewer_maker(filepath, bufnr, opts)
-        end
-    end)
-end
+-- local previewers = require("telescope.previewers")
+-- local new_maker = function(filepath, bufnr, opts)
+--     opts = opts or {}
+--
+--     filepath = vim.fn.expand(filepath)
+--     vim.loop.fs_stat(filepath, function(_, stat)
+--         if not stat then
+--             return
+--         end
+--         if stat.size > 100000 then
+--             return
+--         else
+--             previewers.buffer_previewer_maker(filepath, bufnr, opts)
+--         end
+--     end)
+-- end
 
 -- Telescope setup
 require("telescope").setup({
     defaults = {
         file_ignore_patterns = { ".git/", "dotbot/", "mariadb/data/", "dist/" },
         path_display = { "smart" },
-        buffer_previewer_maker = new_maker,
+        -- buffer_previewer_maker = new_maker,
+        previewer = {
+            filesize_limit = 5,
+        },
         prompt_prefix = " ",
         selection_caret = " ",
         sorting_strategy = "ascending",
