f4beece04acde3c7f8ca747d6387e427fe36ec9f
lando <pluckedowl@gmail.com>
2022-12-07 22:47:27 -0800

    nvim: add treesitter smarty parser

--- nvim/lua/my/plugins/tree-sitter/init.lua
+++ nvim/lua/my/plugins/tree-sitter/init.lua
@@ -99,3 +99,12 @@ require("nvim-treesitter.configs").setup({
     },
     context_commentstring = { enable = true, enable_autocmd = false },
 })
+
+require("nvim-treesitter.parsers").get_parser_configs().smarty = {
+    install_info = {
+        url = "https://github.com/Kibadda/tree-sitter-smarty",
+        files = { "src/parser.c", "src/scanner.cc" },
+        branch = "master",
+    },
+    filetype = "smarty",
+}
