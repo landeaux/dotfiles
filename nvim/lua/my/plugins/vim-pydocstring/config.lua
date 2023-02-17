8bf806588e31bf356ed4614f8a820db4b3d3b14b
Adam Landis <pluckedowl@gmail.com>
2023-02-17 08:40:35 -0800

    nvim: prevent vim-pydocstring from setting default mappings

--- nvim/lua/my/plugins/vim-pydocstring/config.lua
+++ nvim/lua/my/plugins/vim-pydocstring/config.lua
@@ -4,3 +4,4 @@ vim.g.pydocstring_templates_path = vim.fn.stdpath("config")
     .. "/lua/my/plugins/vim-pydocstring/templates/"
     .. formatter
 vim.g.pydocstring_formatter = formatter
+vim.g.pydocstring_enable_mapping = false
