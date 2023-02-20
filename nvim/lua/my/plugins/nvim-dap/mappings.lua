7f10508406dce80d3fca22e60acea8b9eadaee0b
lando <pluckedowl@gmail.com>
2023-02-19 18:37:00 -0800

    nvim: improve DAP mappings

--- nvim/lua/my/plugins/nvim-dap/mappings.lua
+++ nvim/lua/my/plugins/nvim-dap/mappings.lua
@@ -2,55 +2,66 @@ local map = require("my.utils").map_factory({ silent = true })
 
 map("n", "<Leader>dc", function()
     require("dap").continue()
-end, { desc = "Continue" })
+end, { desc = "DAP: Continue" })
+
 map("n", "<Leader>dC", function()
-    require("my.plugins.nvim-dap.utils").reload_continue()
-end, { desc = "Reload continue" })
-map("n", "<Leader>dj", function()
-    require("dap").step_over()
-end, { desc = "Step over" })
-map("n", "<Leader>dl", function()
-    require("dap").step_into()
-end, { desc = "Step into" })
-map("n", "<Leader>dk", function()
-    require("dap").step_out()
-end, { desc = "Step out" })
-map("n", "<Leader>dt", function()
-    require("dap").toggle_breakpoint()
-end, { desc = "Toggle breakpoint" })
-map("n", "<Leader>dbc", function()
-    require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-end, { desc = "Set conditional breakpoint" })
-map("n", "<Leader>dbl", function()
-    require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-end, { desc = "Set breakpoint with log" })
-map("n", "<Leader>dbe", function()
-    require("dap").set_exception_breakpoints({ "all" })
-end, { desc = "Set exception breakpoint" })
+    require("dap").run_last()
+end, { desc = "DAP: Run last" })
+
 map(
     "n",
     "<Leader>dr",
-    ':lua require("dap").repl.open({}, "vsplit")<CR><C-w>l',
-    { desc = "Open DAP REPL" }
+    ':lua require("dap").repl.open({}, "vsplit")<CR><C-w>li',
+    { desc = "DAP: Open DAP REPL" }
 )
-map("n", "<Leader>dR", function()
-    require("dap").run_last()
-end, { desc = "Run last" })
-map("n", "<Leader>d?", function()
-    require("dap.ui.variables").scopes()
-end, { desc = "Variable scopes" })
-map("n", "<Leader>di", function()
-    require("dap.ui.variables").hover(function()
-        return vim.fn.expand("<cexpr>")
-    end)
-end, { desc = "Variable info" })
-map("v", "<Leader>di", function()
-    require("dap.ui.variables").visual_hover()
-end, { desc = "Variable info" })
+
+map("n", "<Leader>dj", function()
+    require("dap").step_over()
+end, { desc = "DAP: Step over" })
+
+map("n", "<Leader>dk", function()
+    require("dap").step_back()
+end, { desc = "DAP: Step back" })
+
+map("n", "<Leader>dl", function()
+    require("dap").step_into()
+end, { desc = "DAP: Step into" })
+
 map("n", "<Leader>dh", function()
-    require("dap.ui.widgets").hover()
-end, { desc = "Widget hover" })
+    require("dap").step_out()
+end, { desc = "DAP: Step out" })
+
+map("n", "<Leader>dt", function()
+    require("dap").toggle_breakpoint()
+end, { desc = "DAP: Toggle breakpoint" })
+
+map("n", "<Leader>dbc", function()
+    require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { desc = "DAP: Set conditional breakpoint" })
+
+map("n", "<Leader>dbl", function()
+    require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end, { desc = "DAP: Set breakpoint with log" })
+
+map("n", "<Leader>dbe", function()
+    require("dap").set_exception_breakpoints({ "all" })
+end, { desc = "DAP: Set exception breakpoint" })
+
+map("n", "<Leader>ds", function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end, { desc = "DAP: View scopes" })
+
 map("n", "<Leader>df", function()
     local widgets = require("dap.ui.widgets")
-    widgets.centered_float(widgets.scopes)
-end, { desc = "Widget float" })
+    widgets.centered_float(widgets.frames)
+end, { desc = "DAP: View frames" })
+
+map("n", "<Leader>dT", function()
+    local widgets = require("dap.ui.widgets")
+    widgets.centered_float(widgets.threads)
+end, { desc = "DAP: View threads" })
+
+map({"n", "v"}, "<Leader>dK", function()
+    require("dap.ui.widgets").hover()
+end, { desc = "DAP: Expression hover" })
