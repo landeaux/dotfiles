4c8473b9a52d3ee0fc09a7a2446cb31dbe3aadde
lando <pluckedowl@gmail.com>
2023-02-21 08:44:09 -0800

    nvim: add nvim-dap-vscode-js

---
+++ nvim/lua/my/plugins/nvim-dap-vscode-js/init.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "mxsdev/nvim-dap-vscode-js",
+        version = "v1.74.1",
+        dependencies = { "mfussenegger/nvim-dap" },
+        config = function()
+            require("my.plugins.nvim-dap-vscode-js.config")
+        end,
+    },
+    {
+        "microsoft/vscode-js-debug",
+        lazy = true,
+        build = "npm install --legacy-peer-deps && npm run compile",
+        tag = "v1.74.1",
+    },
+}
