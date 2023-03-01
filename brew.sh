cca3bc8995edb67996005520c3c0871e9d5e9600
Adam Landis <pluckedowl@gmail.com>
2023-03-01 10:10:23 -0800

    add brew.sh for installing brew packages on MacOS

---
+++ brew.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install a modern version of Bash.
+brew install zsh
+# brew install zsh-completions  # NOTE: may not need with oh-my-zsh
+
+# Switch to using brew-installed zsh as default shell
+if ! grep "${BREW_PREFIX}/bin/zsh" /etc/shells &>/dev/null; then
+  echo "${BREW_PREFIX}/bin/zsh" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/zsh";
+fi;
+
+# Install `wget`
+brew install wget
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+
+# Install other useful binaries.
+brew install ack
+brew install git
+brew install git-lfs
+brew install imagemagick --with-webp
+brew install lynx
+brew install pigz
+brew install rename
+brew install ssh-copy-id
+brew install tree
+# brew install vbindiff  # NOTE: if using, need to update .gitconfig for diffing binaries
+
+# Remove outdated versions from the cellar.
+brew cleanup
