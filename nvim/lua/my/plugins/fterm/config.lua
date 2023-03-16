97e6d49d9f2c0760529bb073d8399c55c768d5df
Adam Landis <pluckedowl@gmail.com>
2023-03-16 10:09:18 -0700

    nvim: add IPython fterm command

--- nvim/lua/my/plugins/fterm/config.lua
+++ nvim/lua/my/plugins/fterm/config.lua
@@ -10,6 +10,10 @@ vim.api.nvim_create_user_command("Python", function()
     require("FTerm").run("pipenv run python")
 end, { bang = true })
 
+vim.api.nvim_create_user_command("IPython", function()
+    require("FTerm").run("pipenv run ipython")
+end, { bang = true })
+
 vim.api.nvim_create_user_command("Node", function()
     require("FTerm").run("node")
 end, { bang = true })
