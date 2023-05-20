40230111ef9ca7668c884acf800e05447ce773bf
lando <pluckedowl@gmail.com>
2023-05-20 11:43:33 -0700

    nvim: add more indent-blankline ft exclusions; remove use_treesitter opt

--- nvim/lua/my/plugins/indent-blankline/config.lua
+++ nvim/lua/my/plugins/indent-blankline/config.lua
@@ -2,15 +2,17 @@ local char = "▏" -- "|", "│",
 
 require("indent_blankline").setup({
     filetype_exclude = {
+        "",
         "checkhealth",
         "git",
         "gitcommit",
         "help",
+        "lazy",
         "lspinfo",
         "man",
         "markdown",
+        "mason",
         "neo-tree",
-        "",
     },
     char = char,
     char_blankline = char,
@@ -18,5 +20,4 @@ require("indent_blankline").setup({
     context_char_blankline = char,
     show_trailing_blankline_indent = false,
     show_current_context = true,
-    use_treesitter = true,
 })
