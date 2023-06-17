cd4c9ab0cd0609177fb4bad19cc9ca6a0bc7a121
lando <pluckedowl@gmail.com>
2023-06-17 12:54:59 -0700

    nvim: install jsregexp for luasnip to get lsp-snippet-transformations

--- nvim/lua/my/plugins/luasnip/init.lua
+++ nvim/lua/my/plugins/luasnip/init.lua
@@ -1,6 +1,9 @@
 return {
     "L3MON4D3/LuaSnip",
     version = "v1.*",
+    -- needed for Variable/Placeholder transformations
+    -- (see: https://code.visualstudio.com/docs/editor/userdefinedsnippets#_variable-transforms)
+    build = "make install_jsregexp",
     config = function()
         require("my.plugins.luasnip.config")
     end,
