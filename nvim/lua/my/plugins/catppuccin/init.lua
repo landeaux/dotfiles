93c226b6571a9e61fc86736d4e99ed9417f11b6b
lando <pluckedowl@gmail.com>
2023-09-16 11:14:22 -0700

    nvim: ensure color scheme is always loaded first

--- nvim/lua/my/plugins/catppuccin/init.lua
+++ nvim/lua/my/plugins/catppuccin/init.lua
@@ -1,6 +1,8 @@
 return {
     "catppuccin/nvim",
     name = "catppuccin",
+    lazy = false, -- load this during startup if it is the main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         require("my.plugins.catppuccin.config")
     end,
