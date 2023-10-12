18d907382802d0aeef2e06b420a29ff9e5a51a05
lando <pluckedowl@gmail.com>
2023-10-11 19:25:04 -0700

    nvim: enable formatting in visual mode

--- nvim/lua/my/plugins/conform-nvim/mappings.lua
+++ nvim/lua/my/plugins/conform-nvim/mappings.lua
@@ -1,6 +1,6 @@
 local conform = require("conform")
 local map = require("my.utils").map_factory({ silent = true })
 
-map("n", "<Leader>lf", function()
+map({ "n", "v" }, "<Leader>lf", function()
     conform.format({ async = true, lsp_fallback = true })
 end, { desc = "Format" })
