1c50c2808d4102e75d2f59f4898e9ba01ea72359
lando <pluckedowl@gmail.com>
2023-10-11 19:10:11 -0700

    nvim: set vim.o.formatexpr to use conform.nvim's formatexpr

--- nvim/lua/my/plugins/conform-nvim/init.lua
+++ nvim/lua/my/plugins/conform-nvim/init.lua
@@ -3,29 +3,32 @@ return {
     init = function()
         require("my.plugins.conform-nvim.mappings")
     end,
-    opts = {
-        formatters_by_ft = {
-            ["_"] = { "trim_whitespace" },
-            ["markdown.mdx"] = { "prettierd" },
-            css = { "prettierd" },
-            graphql = { "prettierd" },
-            handlebars = { "prettierd" },
-            html = { "prettierd" },
-            javascript = { "prettierd", "eslint_d" },
-            javascriptreact = { "prettierd", "eslint_d" },
-            json = { "prettierd" },
-            jsonc = { "prettierd" },
-            less = { "prettierd" },
-            lua = { "stylua" },
-            markdown = { "prettierd" },
-            php = { "php_cs_fixer" },
-            python = { "isort", "black" },
-            scss = { "prettierd" },
-            sh = { "shfmt" },
-            typescript = { "prettierd", "eslint_d" },
-            typescriptreact = { "prettierd", "eslint_d" },
-            vue = { "prettierd", "eslint_d" },
-            yaml = { "prettierd" },
-        },
-    },
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                ["_"] = { "trim_whitespace" },
+                ["markdown.mdx"] = { "prettierd" },
+                css = { "prettierd" },
+                graphql = { "prettierd" },
+                handlebars = { "prettierd" },
+                html = { "prettierd" },
+                javascript = { "prettierd", "eslint_d" },
+                javascriptreact = { "prettierd", "eslint_d" },
+                json = { "prettierd" },
+                jsonc = { "prettierd" },
+                less = { "prettierd" },
+                lua = { "stylua" },
+                markdown = { "prettierd" },
+                php = { "php_cs_fixer" },
+                python = { "isort", "black" },
+                scss = { "prettierd" },
+                sh = { "shfmt" },
+                typescript = { "prettierd", "eslint_d" },
+                typescriptreact = { "prettierd", "eslint_d" },
+                vue = { "prettierd", "eslint_d" },
+                yaml = { "prettierd" },
+            },
+        })
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
 }
