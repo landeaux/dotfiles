833766777a80060157a911f56c576da350b37e4e
Adam Landis <pluckedowl@gmail.com>
2024-03-04 11:57:33 -0800

    nvim: dynamically set @vue/typescript-plugin path

--- nvim/lua/my/plugins/lsp/providers/tsserver.lua
+++ nvim/lua/my/plugins/lsp/providers/tsserver.lua
@@ -1,9 +1,17 @@
+local nvm_dir = vim.env.NVM_DIR
+local node_version = vim.fn.system("node --version"):gsub("%s+$", "")
+
+local vue_typescript_plugin_location = nvm_dir
+    .. "/versions/node/"
+    .. node_version
+    .. "/lib/node_modules/@vue/typescript-plugin"
+
 return {
     init_options = {
         plugins = {
             {
                 name = "@vue/typescript-plugin",
-                location = vim.env.NVM_DIR .. "/versions/node/v18.13.0/lib/node_modules/@vue/typescript-plugin",
+                location = vue_typescript_plugin_location,
                 languages = { "javascript", "typescript", "vue" },
             },
         },
