9cc552e9e7f6cf167ee9cdbeee4aa5c960badb3f
lando <pluckedowl@gmail.com>
2024-03-25 22:09:52 -0700

    nvim: pin neorg to v7.0.0

--- nvim/lua/my/plugins/neorg/init.lua
+++ nvim/lua/my/plugins/neorg/init.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
+    tag = "v7.0.0",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         require("my.plugins.neorg.config")
