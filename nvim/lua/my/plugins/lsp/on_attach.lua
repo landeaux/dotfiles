cc701c42d3ad4bb1ce464b28c7971b07ed58d7c4
Adam Landis <pluckedowl@gmail.com>
2024-05-29 09:31:56 -0700

    nvim(lsp): better name for event arg

--- nvim/lua/my/plugins/lsp/on_attach.lua
+++ nvim/lua/my/plugins/lsp/on_attach.lua
@@ -47,9 +47,9 @@ end
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-        local bufnr = ev.buf
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    callback = function(event)
+        local bufnr = event.buf
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
 
         require("my.plugins.lsp.mappings").register(client, bufnr)
         documentColor(client, bufnr)
