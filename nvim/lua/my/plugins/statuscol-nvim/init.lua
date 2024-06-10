98923917109e2743207e7d3e70dd44f605eb83d3
lando <pluckedowl@gmail.com>
2024-06-09 19:46:34 -0700

    nvim(statuscol): ignore ft=lazy

--- nvim/lua/my/plugins/statuscol-nvim/init.lua
+++ nvim/lua/my/plugins/statuscol-nvim/init.lua
@@ -4,7 +4,7 @@ return {
         local builtin = require("statuscol.builtin")
         require("statuscol").setup({
             relculright = true,
-            ft_ignore = { "gitcommit", "gitrebase", "fugitive", "help", "netrw" },
+            ft_ignore = { "gitcommit", "gitrebase", "fugitive", "help", "lazy", "netrw" },
             segments = {
                 { sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1 }, click = "v:lua.ScSa" },
                 { sign = { namespace = { ".*" }, name = { ".*" }, maxwidth = 1, colwidth = 2 }, click = "v:lua.ScSa" },
