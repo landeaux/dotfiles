7f3bad8eac2df259323c033c3dd53fb37f93c002
lando <pluckedowl@gmail.com>
2024-06-19 10:25:14 -0700

    nvim: various dap improvements
    - remove unused mappings
    - add mappings for setting log point and breakpoint exceptions
    - add step_back mapping
    - use better mapping keys (e.g. <leader>dq to terminate)

--- nvim/lua/my/plugins/dap/init.lua
+++ nvim/lua/my/plugins/dap/init.lua
@@ -1,15 +1,3 @@
----@param config {args?:string[]|fun():string[]?}
-local function get_args(config)
-    local args = type(config.args) == "function" and (config.args() or {}) or config.args or {}
-    config = vim.deepcopy(config)
-    ---@cast args string[]
-    config.args = function()
-        local new_args = vim.fn.input("Run with args: ", table.concat(args, " ")) --[[@as string]]
-        return vim.split(vim.fn.expand(new_args) --[[@as string]], " ")
-    end
-    return config
-end
-
 return {
     {
         "mfussenegger/nvim-dap",
@@ -31,27 +19,37 @@ return {
             end,
         },
         keys = {
-            {
-                "<leader>dB",
-                function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
-                desc = "Breakpoint Condition",
-            },
             { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+            {
+                "<leader>dBc",
+                function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
+                desc = "Set conditional breakpoint",
+            },
+            {
+                "<leader>dBl",
+                function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end,
+                desc = "Set log point",
+            },
+            {
+                "<leader>dBe",
+                function() require("dap").set_exception_breakpoints() end,
+                desc = "Set exception breakpoints",
+            },
             { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
-            { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+            { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
             { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
             { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+            { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
             { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
-            { "<leader>dj", function() require("dap").down() end, desc = "Down" },
-            { "<leader>dk", function() require("dap").up() end, desc = "Up" },
-            { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
             { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
             { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
-            { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+            { "<leader>dh", function() require("dap").step_back() end, desc = "Step Back" },
+            { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+            { "<leader>dk", function() require("dap").up() end, desc = "Up" },
             { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
             { "<leader>ds", function() require("dap").session() end, desc = "Session" },
-            { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
-            { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+            { "<leader>dq", function() require("dap").terminate() end, desc = "Terminate" },
+            { "<leader>dK", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
         },
         config = function()
             local colors = {
@@ -119,12 +117,12 @@ return {
             vscode.json_decode = function(str) return vim.json.decode(json.json_strip_comments(str)) end
 
             -- DAP REPL autocomplete
-            require("my.utils").create_augroup({
-                {
-                    event = "FileType",
-                    opts = { pattern = "dap-repl", command = 'lua require("dap.ext.autocompl").attach()' },
-                },
-            }, "_dap_repl")
+            -- require("my.utils").create_augroup({
+            --     {
+            --         event = "FileType",
+            --         opts = { pattern = "dap-repl", command = 'lua require("dap.ext.autocompl").attach()' },
+            --     },
+            -- }, "_dap_repl")
 
             -- DAP Terminal settings
             -- @see :h dap-terminal
