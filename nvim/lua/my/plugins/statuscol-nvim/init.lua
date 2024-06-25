e8d2767561381565c8deb4fb15c27b479898ebae
Adam Landis <pluckedowl@gmail.com>
2024-06-25 12:14:09 -0700

    nvim(statuscol): ignore ft=DiffviewFiles

--- nvim/lua/my/plugins/statuscol-nvim/init.lua
+++ nvim/lua/my/plugins/statuscol-nvim/init.lua
@@ -4,7 +4,7 @@ return {
         local builtin = require("statuscol.builtin")
         require("statuscol").setup({
             relculright = true,
-            ft_ignore = { "gitcommit", "gitrebase", "fugitive", "help", "lazy", "netrw" },
+            ft_ignore = { "gitcommit", "gitrebase", "fugitive", "help", "lazy", "netrw", "DiffviewFiles" },
             segments = {
                 { sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1 }, click = "v:lua.ScSa" },
                 { sign = { namespace = { ".*" }, name = { ".*" }, maxwidth = 1, colwidth = 2 }, click = "v:lua.ScSa" },
