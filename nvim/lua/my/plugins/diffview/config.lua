bb061c2bec778b0c6c70db77a6f4c36c6b640d5b
Adam Landis <pluckedowl@gmail.com>
2024-06-27 09:38:59 -0700

    nvim(diffview): increase file panel width
    Increase file panel width so git commit window fits nicely and to better
    see paths that have longer file names.

--- nvim/lua/my/plugins/diffview/config.lua
+++ nvim/lua/my/plugins/diffview/config.lua
@@ -2,6 +2,17 @@ local actions = require("diffview.actions")
 
 require("diffview").setup({
     enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+    file_panel = {
+        win_config = {
+            width = 75,
+        }
+    },
+    hooks = {
+        diff_buf_read = function(bufnr)
+            vim.opt_local.relativenumber = false
+            vim.opt_local.wrap = false
+        end,
+    },
     keymaps = {
         view = {
             ["<leader>ge"] = actions.focus_files, -- Bring focus to the file panel
@@ -15,11 +26,5 @@ require("diffview").setup({
             ["<leader>ge"] = actions.focus_files,
             ["<leader>tf"] = actions.toggle_files,
         },
-    },
-    hooks = {
-        diff_buf_read = function(bufnr)
-            vim.opt_local.relativenumber = false
-            vim.opt_local.wrap = false
-        end,
     },
 })
