866f70ac5048183ea5212f2ebc922129615b0b41
lando <pluckedowl@gmail.com>
2024-08-20 16:20:21 -0700

    feat: replace icons for .tar and .snap

--- nvim/lua/my/plugins/nvim-web-devicons/config.lua
+++ nvim/lua/my/plugins/nvim-web-devicons/config.lua
@@ -1,6 +1,6 @@
 require("nvim-web-devicons").setup({
     override = {
-        snap = { icon = "", color = "#7eca9c", name = "snap" },
-        tar = { icon = "", color = "#ebcb8b", name = "tar" },
+        snap = { icon = "󰄀", color = "#7eca9c", name = "snap" },
+        tar = { icon = "󰀼", color = "#ebcb8b", name = "tar" },
     },
 })
