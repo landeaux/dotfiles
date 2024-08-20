489ef54d146d11c80fdc1ea557714eceaa1f493f
lando <pluckedowl@gmail.com>
2024-08-20 16:18:00 -0700

    nvim: use default icons for .zip and .gz

--- nvim/lua/my/plugins/nvim-web-devicons/config.lua
+++ nvim/lua/my/plugins/nvim-web-devicons/config.lua
@@ -1,8 +1,6 @@
 require("nvim-web-devicons").setup({
     override = {
-        gz = { icon = "", color = "#ebcb8b", name = "gz" },
         snap = { icon = "", color = "#7eca9c", name = "snap" },
         tar = { icon = "", color = "#ebcb8b", name = "tar" },
-        zip = { icon = "", color = "#ebcb8b", name = "zip" },
     },
 })
