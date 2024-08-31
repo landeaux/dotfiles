ca31e684757f074b719505cc7b33899f0917f36d
lando <pluckedowl@gmail.com>
2024-08-31 13:15:14 -0700

    nvim: switch to none-ls for linting

---
+++ nvim/lua/my/plugins/none-ls/init.lua
@@ -0,0 +1,18 @@
+return {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.diagnostics.hadolint,
+                require("none-ls.diagnostics.eslint_d"),
+                null_ls.builtins.diagnostics.selene,
+                null_ls.builtins.diagnostics.mypy,
+                null_ls.builtins.diagnostics.markdownlint,
+            },
+        })
+    end,
+}
