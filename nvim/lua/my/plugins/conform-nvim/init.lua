0d03783b0c68e523f431d51712d5bc0e4eff3c59
lando <pluckedowl@gmail.com>
2024-08-30 20:30:25 -0700

    nvim: use correct config for fallback lsp formatting

--- nvim/lua/my/plugins/conform-nvim/init.lua
+++ nvim/lua/my/plugins/conform-nvim/init.lua
@@ -50,7 +50,7 @@ return {
                     ["end"] = { args.line2, end_line:len() },
                 }
             end
-            require("conform").format({ async = true, lsp_fallback = true, range = range })
+            require("conform").format({ async = true, lsp_format = "fallback", range = range })
         end, { range = true })
     end,
 }
