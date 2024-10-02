5ad6bd9e00f33137256a485e4210aa4ea91e9946
lando <pluckedowl@gmail.com>
2024-10-02 09:41:23 -0700

    nvim: rm bad php query injections

--- nvim/after/queries/php/injections.scm
+++
@@ -1,8 +0,0 @@
-; extends
-
-(assignment_expression
-    left: (variable_name (name) @_name (#match? @_name "query"))
-    right: (encapsed_string (string_value) @sql ))
-(assignment_expression
-    left: (variable_name (name) @_name (#match? @_name "sql"))
-    right: (encapsed_string (string_value) @sql ))
