aa5a024eb621b45ba732fb88a9678eb475afaba7
lando <pluckedowl@gmail.com>
2024-10-02 09:38:55 -0700

    nvim: rm commit pin on nvim-treesitter and update

--- nvim/lua/my/plugins/nvim-treesitter/init.lua
+++ nvim/lua/my/plugins/nvim-treesitter/init.lua
@@ -1,7 +1,6 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        commit = "19ac9e8b5c1e5eedd2ae7957243e25b32e269ea7",
         build = ":TSUpdate",
         config = function() require("my.plugins.nvim-treesitter.config") end,
         dependencies = {
@@ -13,7 +12,7 @@ return {
         opts = {
             aliases = {
                 smarty = "html",
-            }
+            },
         },
     },
 }
