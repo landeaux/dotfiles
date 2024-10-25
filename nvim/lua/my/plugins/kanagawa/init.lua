56a3ebd8e0fa0cfac839b235c2bfcd1fa76999da
lando <pluckedowl@gmail.com>
2024-10-25 11:59:04 -0700

    nvim(kanagawa): rm gutter bg; make spell hl underdotted

--- nvim/lua/my/plugins/kanagawa/init.lua
+++ nvim/lua/my/plugins/kanagawa/init.lua
@@ -2,5 +2,27 @@ return {
     "rebelot/kanagawa.nvim",
     lazy = false, -- load this during startup if it is the main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function() vim.cmd.colorscheme("kanagawa-dragon") end,
+    config = function()
+        require("kanagawa").setup({
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none",
+                        },
+                    },
+                },
+            },
+            overrides = function(colors)
+                return {
+                    SpellBad = { underdotted = true },
+                    SpellCap = { underdotted = true },
+                    SpellRare = { underdotted = true },
+                    SpellLocal = { underdotted = true },
+                }
+            end,
+        })
+
+        vim.cmd.colorscheme("kanagawa-dragon")
+    end,
 }
