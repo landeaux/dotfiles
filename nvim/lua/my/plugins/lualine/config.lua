960cc0d9ad0d64820ee930060a75d0bc55352528
Adam Landis <pluckedowl@gmail.com>
2025-03-18 10:34:54 -0700

    nvim(lualine): use new lsp_status component

--- nvim/lua/my/plugins/lualine/config.lua
+++ nvim/lua/my/plugins/lualine/config.lua
@@ -2,21 +2,7 @@ local WIN_BREAKPOINT = 115
 local MAX_BRANCH_LEN = 20
 
 local function win_is_wide() return vim.api.nvim_win_get_width(0) >= WIN_BREAKPOINT end
-
 local function win_is_narrow() return not win_is_wide() end
-
-local function lsp_clients()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local active_clients = vim.tbl_values(vim.lsp.get_clients({ bufnr = bufnr }))
-
-    if vim.tbl_count(active_clients) == 0 then
-        return ""
-    end
-
-    local client_names = vim.tbl_map(function(client) return client.name end, active_clients)
-
-    return "  " .. table.concat(client_names, ", ")
-end
 
 local function truncate(str, max_length)
     if str:len() > max_length then
@@ -52,11 +38,11 @@ require("lualine").setup({
             { "filename", path = 4, cond = win_is_narrow },
         },
         lualine_x = {
-            { lsp_clients, cond = win_is_wide },
+            { "lsp_status", icon = " ", cond = win_is_wide },
             { "filetype", cond = win_is_wide },
         },
         lualine_y = {
-            { lsp_clients, cond = win_is_narrow },
+            { "lsp_status", icon = " ", cond = win_is_narrow },
             { "progress", cond = win_is_wide },
         },
         lualine_z = {
