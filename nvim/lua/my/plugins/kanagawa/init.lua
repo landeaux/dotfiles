1d872aa71891e79163388ffdc662ca605cb65725
lando <pluckedowl@gmail.com>
2025-04-06 08:36:16 -0700

    nvim(kanagawa): restore dark popup menu; fix highlights for blink

--- nvim/lua/my/plugins/kanagawa/init.lua
+++ nvim/lua/my/plugins/kanagawa/init.lua
@@ -15,7 +15,20 @@ return {
                 },
             },
             overrides = function(colors)
+                local theme = colors.theme
                 return {
+                    -- dark popup menu
+                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                    PmenuSbar = { bg = theme.ui.bg_m1 },
+                    PmenuThumb = { bg = theme.ui.bg_p2 },
+
+                    -- fix blink.cmp highlights (see https://github.com/rebelot/kanagawa.nvim/issues/283#issuecomment-2766568473)
+                    BlinkCmpMenu = { bg = colors.palette.dragonBlack3 },
+                    BlinkCmpMenuBorder = { bg = colors.palette.dragonBlack3 },
+                    BlinkCmpLabelDetail = { bg = colors.palette.dragonBlack3 },
+                    BlinkCmpMenuSelection = { bg = colors.palette.waveBlue1 },
+
                     -- spelling
                     SpellBad = { underdotted = true },
                     SpellCap = { underdotted = true },
