11dda975dafae3ad02c47e993cc42769c5db6439
lando <pluckedowl@gmail.com>
2025-04-05 19:52:44 -0700

    nvim(kanagawa): revert dark popup menu highlights and rely on defaults

--- nvim/lua/my/plugins/kanagawa/init.lua
+++ nvim/lua/my/plugins/kanagawa/init.lua
@@ -15,14 +15,7 @@ return {
                 },
             },
             overrides = function(colors)
-                local theme = colors.theme
                 return {
-                    -- dark popup menu
-                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-                    PmenuSbar = { bg = theme.ui.bg_m1 },
-                    PmenuThumb = { bg = theme.ui.bg_p2 },
-
                     -- spelling
                     SpellBad = { underdotted = true },
                     SpellCap = { underdotted = true },
