45508a70419c93049a69e3524c194fb4a18c8042
lando <pluckedowl@gmail.com>
2025-04-06 09:56:02 -0700

    nvim(lazydev): use vendored luvit-meta lib

--- nvim/lua/my/plugins/lazydev-nvim/init.lua
+++ nvim/lua/my/plugins/lazydev-nvim/init.lua
@@ -4,11 +4,10 @@ return {
         ft = "lua",
         opts = {
             library = {
-                { path = "luvit-meta/library", words = { "vim%.uv" } },
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
             },
         },
     },
-    { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings }
     { -- optional blink completion source for require statements and module annotations
         "saghen/blink.cmp",
         opts = {
