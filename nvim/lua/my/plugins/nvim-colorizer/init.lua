55c3e4710cefaa1df9ff12b93702a993a84dfff4
Adam Landis <pluckedowl@gmail.com>
2025-05-12 10:59:11 -0700

    nvim(nvim-colorizer): disable color names

--- nvim/lua/my/plugins/nvim-colorizer/init.lua
+++ nvim/lua/my/plugins/nvim-colorizer/init.lua
@@ -1,7 +1,11 @@
 return {
     "catgoose/nvim-colorizer.lua",
     init = function() require("my.plugins.nvim-colorizer.mappings") end,
-    opts = {},
+    opts = {
+        user_default_options = {
+            names = false,
+        },
+    },
     cmd = {
         "ColorizerAttachToBuffer",
         "ColorizerDetachFromBuffer",
