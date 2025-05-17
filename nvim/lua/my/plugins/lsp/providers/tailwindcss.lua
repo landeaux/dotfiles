bdefb05a525bcc00976bdc036b8b31b31828b9d9
lando <pluckedowl@gmail.com>
2025-05-17 13:35:03 -0700

    nvim(lsp): allow tailwind to recognize RouterLink class props

---
+++ nvim/lua/my/plugins/lsp/providers/tailwindcss.lua
@@ -0,0 +1,15 @@
+return {
+    settings = {
+        tailwindCSS = {
+            classAttributes = {
+                "class",
+                "className",
+                "class:list",
+                "classList",
+                "ngClass",
+                "active-class", -- For Vue Router RouterLink component
+                "exact-active-class", -- For Vue Router RouterLink component
+            },
+        },
+    },
+}
