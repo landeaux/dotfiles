2a0cb55f08425511a8ce3ce8046eaa5d5a3ce8b7
Adam Landis <pluckedowl@gmail.com>
2025-05-29 09:40:41 -0700

    nvim(treesitter): ensure nvim-treesitter not lazily loaded

--- nvim/lua/my/plugins/nvim-treesitter/init.lua
+++ nvim/lua/my/plugins/nvim-treesitter/init.lua
@@ -3,6 +3,7 @@ return {
         "nvim-treesitter/nvim-treesitter",
         branch = "master", -- TODO: migrate to main branch
         build = ":TSUpdate",
+        lazy = false,
         config = function() require("my.plugins.nvim-treesitter.config") end,
         dependencies = {
             "nvim-treesitter/nvim-treesitter-textobjects",
