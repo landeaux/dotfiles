a7c2d2e223d94e3ee887c4adef8fff3aa14eaab5
Adam Landis <pluckedowl@gmail.com>
2025-07-28 09:09:40 -0700

    nvim/kitty: update kitty-scrollback-nvim to v6

--- nvim/lua/my/plugins/kitty-scrollback-nvim/init.lua
+++ nvim/lua/my/plugins/kitty-scrollback-nvim/init.lua
@@ -4,6 +4,6 @@ return {
     lazy = true,
     cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
     event = { "User KittyScrollbackLaunch" },
-    version = "^5.0.0", -- latest stable version, may have breaking changes if major version changed
+    version = "^6.0.0", -- latest stable version, may have breaking changes if major version changed
     config = function() require("kitty-scrollback").setup() end,
 }
