fe23fce8de07341d0ed7eeab09a54a414b213cea
Adam Landis <pluckedowl@gmail.com>
2025-07-28 09:12:53 -0700

    nvim: lazy-load on KittyScrollbackGenerateCommandLineEditing event

--- nvim/lua/my/plugins/kitty-scrollback-nvim/init.lua
+++ nvim/lua/my/plugins/kitty-scrollback-nvim/init.lua
@@ -2,7 +2,11 @@ return {
     "mikesmithgh/kitty-scrollback.nvim",
     enabled = true,
     lazy = true,
-    cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+    cmd = {
+        "KittyScrollbackGenerateKittens",
+        "KittyScrollbackCheckHealth",
+        "KittyScrollbackGenerateCommandLineEditing",
+    },
     event = { "User KittyScrollbackLaunch" },
     version = "^6.0.0", -- latest stable version, may have breaking changes if major version changed
     config = function() require("kitty-scrollback").setup() end,
