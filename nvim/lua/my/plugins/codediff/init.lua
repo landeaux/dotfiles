84321620e275161581f3c7fcbe8adc078c3f1fb3
Adam Landis <pluckedowl@gmail.com>
2026-01-28 14:12:08 -0800

    feat(nvim): configure codediff to focus on modified buffer

--- nvim/lua/my/plugins/codediff/init.lua
+++ nvim/lua/my/plugins/codediff/init.lua
@@ -2,4 +2,10 @@ return {
     "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     cmd = "CodeDiff",
+    opts = {
+        -- Explorer panel configuration
+        explorer = {
+            initial_focus = "modified", -- Initial focus: "explorer", "original", or "modified"
+        },
+    },
 }
