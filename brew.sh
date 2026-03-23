4620062f8956f00ad42554909165875cc2a6d088
Adam Landis <pluckedowl@gmail.com>
2026-03-23 11:30:05 -0700

    refactor: replace individual brew installs with Brewfile
    Move package declarations to a Brewfile managed by `brew bundle` instead
    of individual `brew install` commands in brew.sh.

--- brew.sh
+++ brew.sh
@@ -2,96 +2,23 @@
 
 # Install command-line tools using Homebrew.
 
-# Make sure we’re using the latest Homebrew.
+# Make sure we're using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Save Homebrew’s installed location.
+# Install everything from the Brewfile.
+brew bundle --file=~/dotfiles/Brewfile
+
+# Save Homebrew's installed location.
 BREW_PREFIX=$(brew --prefix)
-
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install vim
-brew install grep
-brew install openssh
-brew install screen
-brew install php
-
-# Install other useful binaries.
-brew install ack
-brew install bat
-brew install direnv
-brew install entr
-brew install fd
-brew install git
-brew install git-extras
-brew install git-lfs
-brew install glab
-brew install goku # for writing karabiner.json
-brew install hyperfine
-brew install jq
-brew install lua
-brew install luarocks
-brew install neovim
-brew install pipx
-brew install rename
-brew install ripgrep
-brew install ssh-copy-id
-brew install starship # for shell prompt
-brew install stylua
-brew install task
-brew install tmux
-brew install tree
-brew install tree-sitter
-brew install tree-sitter-cli
-brew install wget
-brew install pngpaste # for https://github.com/epwalsh/obsidian.nvim?tab=readme-ov-file#setup
-
-# Install yabai (tiling window manager) and skhd (hotkey daemon)
-brew install koekeishiya/formulae/yabai
-brew install koekeishiya/formulae/skhd
-
-# Install qmk
-brew install qmk/qmk/qmk
-
-# Install pyenv dependencies
-brew install libb2 # Fixes blake2b hash issue. See https://github.com/pyenv/pyenv/issues/2573#issuecomment-1415335052
-# Install pyenv python build dependencies
-brew install openssl readline sqlite3 xz zlib tcl-tk
-
-# Install fzf
-brew install fzf
-
-# Install Fonts
-brew install --cask font-jetbrains-mono
-brew install --cask font-symbols-only-nerd-font
-
-# Install a modern version of Zsh.
-brew install zsh
-# brew install zsh-completions  # NOTE: may not need with oh-my-zsh
 
 # Switch to using brew-installed zsh as default shell
 if ! grep "${BREW_PREFIX}/bin/zsh" /etc/shells &>/dev/null; then
   echo "${BREW_PREFIX}/bin/zsh" | sudo tee -a /etc/shells
   chsh -s "${BREW_PREFIX}/bin/zsh"
 fi
-
-# Install kitty terminal
-brew install --cask kitty
 
 # Remove outdated versions from the cellar.
 brew cleanup
