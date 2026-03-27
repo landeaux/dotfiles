223f4471c3f198ba19544e2f714061a5c0f94d3c
Adam Landis <pluckedowl@gmail.com>
2026-03-27 15:43:18 -0700

    feat(zsh): add misc tool hooks (direnv, bat)

---
+++ zsh/conf.d/08-tools.zsh
@@ -0,0 +1,9 @@
+# direnv
+if command -v direnv 1>/dev/null 2>&1; then
+  eval "$(direnv hook zsh)"
+fi
+
+# bat as manpager
+if command -v bat 1>/dev/null 2>&1; then
+  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+fi
