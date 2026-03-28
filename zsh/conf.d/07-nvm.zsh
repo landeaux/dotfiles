37bd3666e700765115705546a892e17886fadbd3
lando <pluckedowl@gmail.com>
2026-03-28 13:26:24 -0700

    refactor(zsh): source nvm from ~/.nvm instead of brew prefix

--- zsh/conf.d/07-nvm.zsh
+++ zsh/conf.d/07-nvm.zsh
@@ -1,12 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
-
-if command -v brew 1>/dev/null 2>&1; then
-  local _brew_prefix
-  _brew_prefix="$(brew --prefix)"
-  [ -s "$_brew_prefix/opt/nvm/nvm.sh" ] && \. "$_brew_prefix/opt/nvm/nvm.sh"
-  [ -s "$_brew_prefix/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$_brew_prefix/opt/nvm/etc/bash_completion.d/nvm"
-  unset _brew_prefix
-fi
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Auto-switch Node versions when entering a directory with .nvmrc
 autoload -U add-zsh-hook
