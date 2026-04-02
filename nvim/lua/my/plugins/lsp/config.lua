8021700967f0d0364523133989595040caf96eae
Adam Landis <pluckedowl@gmail.com>
2026-04-02 15:49:11 -0700

    feat(nvim): show LSP progress via nvim_echo

--- nvim/lua/my/plugins/lsp/config.lua
+++ nvim/lua/my/plugins/lsp/config.lua
@@ -1,2 +1,16 @@
 require("my.plugins.lsp.providers")
 require("my.plugins.lsp.on_attach")
+
+vim.api.nvim_create_autocmd("LspProgress", {
+    callback = function(ev)
+        local value = ev.data.params.value
+        vim.api.nvim_echo({ { value.message or "done" } }, false, {
+            id = "lsp." .. ev.data.client_id,
+            kind = "progress",
+            source = "vim.lsp",
+            title = value.title,
+            status = value.kind ~= "end" and "running" or "success",
+            percent = value.percentage,
+        })
+    end,
+})
